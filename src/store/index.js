31e9d3e3c1ded84d7cb7e8efcd5a0787265b3ebb
BlackStump <altocoey@hotmail.com>
2020-05-03 13:08:22 +1000

    Update index.js
    heater==>heaters

--- src/store/index.js
+++ src/store/index.js
@@ -90,7 +90,7 @@ export default new Vuex.Store({
             endstops: {},
         },
         object: {
-            heater: {
+            heaters: {
                 available_heaters: []
             }
         },
