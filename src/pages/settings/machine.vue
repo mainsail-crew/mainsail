9ccce80a839918be403637dca8bb5b70505edf16
Stefan Dej <meteyou@gmail.com>
2020-08-30 14:58:26 +0200

    Update all klipper/moonraker communication to Moonraker 'August 16 2020' Version. The Klippy state is more definied and should work better now. This work is not finished!
    feature: display print complete with time and filament used + reprint button
    
    bugfix: Fix issue #48. Tempchart need to many load
    bugfix: Fix issue #46. Print time > 1 day (was a moonraker bug) + format time to "1d 1h" and not "25h"
    bugfix: Fix issue #39. Mainsail title will show filename
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

--- src/pages/settings/machine.vue
+++ src/pages/settings/machine.vue
@@ -6,12 +6,12 @@
             </v-col>
             <v-col class="col-sm-12 col-md-6">
                 <v-row>
-                    <v-col class="col-sm-12 pt-0" v-if="is_ready">
+                    <v-col class="col-sm-12 pt-0" v-if="klippy_state === 'ready'">
                         <settings-limits-panel></settings-limits-panel>
                     </v-col>
                 </v-row>
                 <v-row>
-                    <v-col class="col-sm-12 col-md-6" v-if="is_ready">
+                    <v-col class="col-sm-12 col-md-6" v-if="klippy_state === 'ready'">
                         <settings-endstop-panel></settings-endstop-panel>
                         <settings-runout-panel></settings-runout-panel>
                     </v-col>
@@ -34,7 +34,7 @@
         }),
         computed: {
             ...mapState({
-                is_ready: state => state.socket.is_ready,
+                klippy_state: state => state.printer.webhooks.state,
             }),
         },
         created() {
