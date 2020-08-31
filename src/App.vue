09f46ae7857f077f0ffbedf559565f1fd7a556fd
Stefan Dej <meteyou@gmail.com>
2020-08-31 23:18:23 +0200

    insert version number
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -76,7 +76,8 @@
         </v-dialog>
 
         <v-footer app class="d-block">
-            <span v-if="version">{{ version }}</span>
+            <span>v0.2.0</span>
+            <span v-if="version"> - {{ version }}</span>
             <span class="float-right">Made with <img src="/img/heard.png" height="15" title="love" alt="heard" /> by <a href="http://www.vorondesign.com/" target="_blank"><img src="/img/voron.png" height="15" title="VoronDesign" alt="Logo - VoronDesign" /></a></span>
         </v-footer>
     </v-app>
