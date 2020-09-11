a470c151816e01ce4debb55a5baaa8103643994d
Craig Bassett <craig.bassett@gmail.com>
2020-09-11 14:07:00 -0500

    feature: allows flipping the webcam horizontally and vertically

--- src/components/panels/Settings/WebcamPanel.vue
+++ src/components/panels/Settings/WebcamPanel.vue
@@ -1,7 +1,5 @@
 <style>
-    .webcamImage {
-        width: 100%;
-    }
+    .content { overflow: hidden; }
 </style>
 
 <template>
@@ -13,18 +11,27 @@
             </v-list-item-content>
         </v-list-item>
         <v-divider class="my-2"></v-divider>
-        <v-card-text class="px-0 pb-0 pt-3 content">
+        <v-card-text class="px-0 pt-3 content">
             <v-row>
                 <v-col class="px-10 py-0">
                     <v-text-field
                         v-model="webcamUrl"
+                        hide-details
                         label="Webcam URL"
                     ></v-text-field>
-                </v-col>
-            </v-row>
-            <v-row>
-                <v-col class="px-10 py-0">
-                    <v-switch v-model="boolNavi" label="Show in Navigation"></v-switch>
+
+                    <v-row v-if="rotationEnabled">
+                        <v-col sm="auto">
+                            <v-switch v-model="rotate" hide-details label="Rotate"></v-switch>
+                        </v-col>
+                        <v-col>
+                            <v-select :items="[{ text: '90 degrees', value: 90 }, { text: '270 degrees', value: 270 }]" v-model="rotateDegrees" hide-details></v-select>
+                        </v-col>
+                    </v-row>
+                    
+                    <v-switch v-model="flipX" hide-details label="Flip webcam horizontally"></v-switch>
+                    <v-switch v-model="flipY" hide-details label="Flip webcam vertically"></v-switch>
+                    <v-switch v-model="boolNavi" hide-details label="Show in navigation"></v-switch>
                 </v-col>
             </v-row>
         </v-card-text>
@@ -38,24 +45,56 @@
         },
         data: function() {
             return {
-
+                rotationEnabled: false
             }
         },
         computed: {
             webcamUrl: {
                 get() {
-                    return this.$store.state.webcam.url;
+                    return this.$store.state.gui.webcam.url;
                 },
-                set(newWebcamUrl) {
-                    return this.$store.dispatch('setSettings', { webcam: { url: newWebcamUrl } });
+                set(url) {
+                    return this.$store.dispatch('setSettings', { gui: { webcam: { url } } });
+                }
+            },
+            flipX: {
+                get() {
+                    return this.$store.state.gui.webcam.flipX;
+                },
+                set(flipX) {
+                    return this.$store.dispatch('setSettings', { gui: { webcam: { flipX } } });
+                }
+            },
+            flipY: {
+                get() {
+                    return this.$store.state.gui.webcam.flipY;
+                },
+                set(flipY) {
+                    return this.$store.dispatch('setSettings', { gui: { webcam: { flipY } } });
+                }
+            },
+            rotate: {
+                get() {
+                    return this.$store.state.gui.webcam.rotate;
+                },
+                set(rotate) {
+                    return this.$store.dispatch('setSettings', { gui: { webcam: { rotate } } });
+                }
+            },
+            rotateDegrees: {
+                get() {
+                    return this.$store.state.gui.webcam.rotateDegrees;
+                },
+                set(rotateDegrees) {
+                    return this.$store.dispatch('setSettings', { gui: { webcam: { rotateDegrees } } });
                 }
             },
             boolNavi: {
                 get() {
                     return this.$store.state.gui.webcam.bool;
                 },
-                set(bool) {
-                    return this.$store.dispatch('setSettings', { gui: { webcam: { bool: bool } } });
+                set(showNav) {
+                    return this.$store.dispatch('setSettings', { gui: { webcam: { bool: showNav } } });
                 }
             },
         },
