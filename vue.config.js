b27390133a5744f002696bb16c2487b964535f4b
Stefan Dej <meteyou@gmail.com>
2020-10-15 21:09:49 +0200

    feature: read version from package.json
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,21 @@
+const webpack = require('webpack')
+const fs = require('fs')
+const packageJson = fs.readFileSync('./package.json')
+const version = JSON.parse(packageJson).version || 0
+
 module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': {
+          PACKAGE_VERSION: '"' + version + '"'
+        }
+      })
+    ]
+  },
   pwa: {
     serviceWorker:false,
     iconPaths: {
