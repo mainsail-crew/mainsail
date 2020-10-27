3d70cf957834581a564c6b4e4fd32f3bf044404d
Stefan Dej <meteyou@gmail.com>
2020-10-27 22:44:02 +0100

    bugfix: machine-limits expander
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

--- src/inputs/ToolSlider.vue
+++ src/inputs/ToolSlider.vue
@@ -4,9 +4,29 @@
 
 <template>
     <v-row>
-        <v-col class="col-auto"><v-icon @click="decrement">mdi-minus</v-icon></v-col>
-        <v-col class="col"><v-slider v-model="value" thumb-label="always" :label="label" :min="min" :max="variableMax" @change="sendCmd" hide-details></v-slider></v-col>
-        <v-col class="col-auto"><v-icon @click="increment">mdi-plus</v-icon></v-col>
+        <v-col class="py-1">
+            <v-slider
+                v-model="value"
+                :label="label"
+                :min="min"
+                :max="max"
+                @change="sendCmd"
+                :prepend-icon="prependIcon"
+                hide-details>
+
+                <template v-slot:append>
+                    <v-text-field
+                        v-model="value"
+                        class="mt-0 pt-0"
+                        hide-details
+                        single-line
+                        type="number"
+                        style="width: 80px"
+                        @change="sendCmd"
+                    ></v-text-field>
+                </template>
+            </v-slider>
+        </v-col>
     </v-row>
 </template>
 
@@ -16,7 +36,6 @@
         data: function() {
             return {
                 value: this.target * this.multi,
-                variableMax: this.max,
             }
         },
         props: {
@@ -34,6 +53,11 @@
                 default: ''
             },
             label: {
+                type: String,
+                required: false,
+                default: ''
+            },
+            prependIcon: {
                 type: String,
                 required: false,
                 default: ''
@@ -76,45 +100,13 @@
             sendCmd() {
                 this.$socket.sendObj('printer.gcode.script', { script: this.command+' '+this.attributeName+(this.value*this.attributeScale).toFixed(0) });
             },
-            decrement() {
-                this.value--;
-                this.sendCmd();
-            },
-            increment() {
-                this.value++;
-                this.sendCmd();
-            },
-            checkExpand() {
-                if (this.value > 0) {
-                    if (this.value > this.variableMax) {
-                        let tmpMulti = Math.ceil((this.value - this.variableMax) / this.extenderSteps);
-                        this.variableMax += tmpMulti * this.extenderSteps;
-                    } else if (this.value > (this.variableMax - this.extenderSteps)) {
-                        this.variableMax += this.extenderSteps;
-                    }
-                }
-            }
         },
         watch: {
             target: function() {
                 this.value = this.target * this.multi;
             },
-            value: function() {
-                if (this.extender) {
-
-                    setTimeout(() => {
-                        this.checkExpand();
-                    }, 1000);
-                }
-            }
         },
         created: function() {
-            if (this.extender && this.value > this.variableMax) {
-                let tmpMulti = Math.ceil((this.value - this.variableMax) / this.extenderSteps);
-                this.variableMax += tmpMulti * this.extenderSteps;
-
-                if (this.variableMax === this.value) this.variableMax += this.extenderSteps;
-            }
         }
     }
 </script>
