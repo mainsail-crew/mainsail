4b14a17e56a1e9dec4d53dc067a96ac87f0fc701
eliteSCHW31N <admin@elitesch31n.de>
2020-12-15 14:45:01 +0100

    add neopixelcenter functions

--- src/components/panels/Settings/NeopixelCenterPanel.vue
+++ src/components/panels/Settings/NeopixelCenterPanel.vue
@@ -14,7 +14,7 @@
         </v-toolbar>
         <v-card-text>
             <v-row>
-                <v-col class="py-0">
+                <v-col class="py-0 pb-3">
                     <v-text-field
                         v-model="numbleds"
                         hide-details
@@ -39,6 +39,7 @@
 
 <script>
     import {bus} from "../../../main";
+    const axios = require('axios');
     
     export default {
         components: {
@@ -58,6 +59,16 @@
                     return this.$store.state.gui.neopixelcenter.numbleds;
                 },
                 set(numbleds) {
+                    var URL = this.$store.state.gui.modules.neopixelcenterUrl;
+                    if(URL.startsWith("https://")||URL.startsWith("http://")){
+                        axios.get(URL+"/updateLeds?amount="+numbleds)
+                        .then(function (){
+                            
+                        })
+                        .catch(function (){
+                            
+                        });
+                    }
                     return this.$store.dispatch('gui/setSettings', { neopixelcenter: { numbleds } });
                 }
             },
@@ -66,7 +77,19 @@
                     return this.$store.state.gui.neopixelcenter.color;
                 },
                 set(color) {
-                    console.log(this.hexAToRGBA(color))
+                    var URL = this.$store.state.gui.modules.neopixelcenterUrl;
+                    var ledmaxid = this.$store.state.gui.neopixelcenter.numbleds;
+                    ledmaxid=ledmaxid - 1;
+                    var ledcolor = this.hexAToRGBA(color);
+                    if(URL.startsWith("https://")||URL.startsWith("http://")){
+                        axios.get(URL+"/changeLedInRange?from=0&to="+ledmaxid+"&red="+ledcolor[0]+"&green="+ledcolor[1]+"&blue="+ledcolor[2]+"&brightness="+ledcolor[3]+"")
+                        .then(function (){
+                            
+                        })
+                        .catch(function (){
+                            
+                        });
+                    }
                     return this.$store.dispatch('gui/setSettings', { neopixelcenter: { color } });
                 }
             },
@@ -90,8 +113,9 @@
                 a = +(a / 255)
                 a = a*100
                 a = a.toFixed(0);
+                var ledcolor = [+r,+g,+b,+a]
 
-                return "rgba(" + +r + "," + +g + "," + +b + "," + a + ")";
+                return ledcolor;
             },
             show:function(e){
                 bus.$emit("showkeyboard",e);
--- src/neopixelcenter/index.js
+++ src/neopixelcenter/index.js
@@ -2,9 +2,60 @@ import store from '../store/index'
 
 const axios = require('axios');
 
+var URL = store.state.gui.modules.neopixelcenterUrl;
+
+var numbleds = store.state.gui.neopixelcenter.numbleds;
+
+
+if(URL.startsWith("https://")||URL.startsWith("http://")){
+    axios.get(URL+"/updateLeds?amount="+numbleds)
+    .then(function (){
+        
+    })
+    .catch(function (){
+        
+    });
+}
+
+var ledmaxid = store.state.gui.neopixelcenter.numbleds;
+ledmaxid=ledmaxid - 1;
+var ledcolor = hexAToRGBA(store.state.gui.neopixelcenter.color);
+if(URL.startsWith("https://")||URL.startsWith("http://")){
+    axios.get(URL+"/changeLedInRange?from=0&to="+ledmaxid+"&red="+ledcolor[0]+"&green="+ledcolor[1]+"&blue="+ledcolor[2]+"&brightness="+ledcolor[3]+"")
+    .then(function (){
+        
+    })
+    .catch(function (){
+        
+    });
+}
+
 setInterval(retrieveData,1000);
+
+function hexAToRGBA(h) {
+    let r = 0, g = 0, b = 0, a = 1;
+
+    if (h.length == 5) {
+        r = "0x" + h[1] + h[1];
+        g = "0x" + h[2] + h[2];
+        b = "0x" + h[3] + h[3];
+        a = "0x" + h[4] + h[4];
+
+    } else if (h.length == 9) {
+        r = "0x" + h[1] + h[2];
+        g = "0x" + h[3] + h[4];
+        b = "0x" + h[5] + h[6];
+        a = "0x" + h[7] + h[8];
+    }
+    a = +(a / 255)
+    a = a*100
+    a = a.toFixed(0);
+    var ledcolor = [+r,+g,+b,+a]
+
+    return ledcolor;
+}
+
 function retrieveData(){
-    var URL = store.state.gui.modules.neopixelcenterUrl;
     if(!URL.startsWith("https://")&&!URL.startsWith("http://")){
         return;
     }
