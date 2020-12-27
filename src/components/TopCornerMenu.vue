daa4fd82a066bcf4860d68207ababb8a4536a722
Stefan Dej <meteyou@gmail.com>
2020-12-27 21:35:00 +0100

    make top corner menu smaller
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

--- src/components/TopCornerMenu.vue
+++ src/components/TopCornerMenu.vue
@@ -1,3 +1,9 @@
+<style scoped>
+    .minheight30 {
+        min-height: 30px;
+    }
+</style>
+
 <template>
     <v-menu bottom left :offset-y="true" :close-on-content-click="false">
         <template v-slot:activator="{ on, attrs }">
@@ -7,37 +13,37 @@
         </template>
 
         <v-list dense>
-            <v-subheader>Klipper Control</v-subheader>
-            <v-list-item link @click="doRestart()">
-                <v-list-item-title><v-icon class="mr-3">mdi-sync</v-icon>Restart</v-list-item-title>
+            <v-subheader class="" style="height: auto;">Klipper Control</v-subheader>
+            <v-list-item class="minheight30" link @click="doRestart()">
+                <v-list-item-title><v-icon class="mr-2" small>mdi-sync</v-icon>Restart</v-list-item-title>
             </v-list-item>
-            <v-list-item link @click="doFirmwareRestart()">
-                <v-list-item-title><v-icon class="mr-3">mdi-sync</v-icon>Firmware Restart</v-list-item-title>
+            <v-list-item class="minheight30" link @click="doFirmwareRestart()">
+                <v-list-item-title><v-icon class="mr-2" small>mdi-sync</v-icon>Firmware Restart</v-list-item-title>
             </v-list-item>
-            <v-divider class="mt-2"></v-divider>
-            <v-subheader>Restart Services</v-subheader>
-            <v-list-item link @click="doServiceRestartKlipper()">
-                <v-list-item-title><v-icon class="mr-3">mdi-sync</v-icon>Klipper</v-list-item-title>
+            <v-divider class="mt-0"></v-divider>
+            <v-subheader class="pt-2" style="height: auto;">Restart Services</v-subheader>
+            <v-list-item class="minheight30"  link @click="doServiceRestartKlipper()">
+                <v-list-item-title><v-icon class="mr-2" small>mdi-sync</v-icon>Klipper</v-list-item-title>
             </v-list-item>
-            <v-list-item link @click="doServiceRestartMoonraker()">
-                <v-list-item-title><v-icon class="mr-3">mdi-sync</v-icon>Moonraker</v-list-item-title>
+            <v-list-item class="minheight30"  link @click="doServiceRestartMoonraker()">
+                <v-list-item-title><v-icon class="mr-2" small>mdi-sync</v-icon>Moonraker</v-list-item-title>
             </v-list-item>
             <div v-if="countPowerDevices">
-                <v-divider class="mt-2"></v-divider>
-                <v-subheader>Power Devices</v-subheader>
-                <v-list-item v-for="(device, index) in devices" v-bind:key="index" @click="changeSwitch(device, device.status)" :disabled="(device.status === 'error')">
+                <v-divider class="mt-0"></v-divider>
+                <v-subheader class="pt-2" style="height: auto;">Power Devices</v-subheader>
+                <v-list-item v-for="(device, index) in devices" v-bind:key="index" class="minheight30" @click="changeSwitch(device, device.status)" :disabled="(device.status === 'error')">
                     <v-list-item-title>
-                        <v-icon class="mr-3">mdi-{{ device.status === 'on' ? 'toggle-switch' : 'toggle-switch-off' }}</v-icon>{{ device.device }}
+                        <v-icon class="mr-2">mdi-{{ device.status === 'on' ? 'toggle-switch' : 'toggle-switch-off' }}</v-icon>{{ device.device }}
                     </v-list-item-title>
                 </v-list-item>
             </div>
-            <v-divider class="mt-2"></v-divider>
-            <v-subheader>Host Control</v-subheader>
-            <v-list-item link @click="doHostReboot()">
-                <v-list-item-title><v-icon class="mr-3">mdi-power</v-icon>Reboot</v-list-item-title>
+            <v-divider class="mt-0"></v-divider>
+            <v-subheader class="pt-2" style="height: auto;">Host Control</v-subheader>
+            <v-list-item class="minheight30" link @click="doHostReboot()">
+                <v-list-item-title><v-icon class="mr-2" small>mdi-power</v-icon>Reboot</v-list-item-title>
             </v-list-item>
-            <v-list-item link @click="doHostShutdown()">
-                <v-list-item-title><v-icon class="mr-3">mdi-power</v-icon>Shutdown</v-list-item-title>
+            <v-list-item class="minheight30" link @click="doHostShutdown()">
+                <v-list-item-title><v-icon class="mr-2" small>mdi-power</v-icon>Shutdown</v-list-item-title>
             </v-list-item>
         </v-list>
     </v-menu>
@@ -90,7 +96,3 @@ export default {
     }
 }
 </script>
-
-<style scoped>
-
-</style>
