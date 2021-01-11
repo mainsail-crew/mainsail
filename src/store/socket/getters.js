84f2b6a4731aaa43d6a553853e7f17cde13a10b2
Stefan Dej <meteyou@gmail.com>
2021-01-11 22:48:23 +0100

    bugfix: switching printers in remoteMode change to single mode
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

--- src/store/socket/getters.js
+++ src/store/socket/getters.js
@@ -1,6 +1,6 @@
 export default {
 
 	getUrl: state => {
-		return "//" + state.hostname + (state.port === 80 ? ":"+state.port : "")
+		return "//" + state.hostname + (state.port !== 80 ? ":"+state.port : "")
 	}
 }
