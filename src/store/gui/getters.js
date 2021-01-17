5d5bdcc6198879dc5e5805afe4473231a979d8cb
Stefan Dej <meteyou@gmail.com>
2021-01-17 01:32:26 +0100

    feature: add preheat & cooldown function in ToolsPanel.vue
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

--- src/store/gui/getters.js
+++ src/store/gui/getters.js
@@ -1,3 +1,14 @@
+import {caseInsensitiveNameSort} from "@/plugins/helpers";
+
 export default {
 
+	getPreheatPresets:(state) => {
+		const output = []
+
+		for (const [key, preset] of Object.entries(state.presets)) {
+			output.push({ ...preset, index: key })
+		}
+
+		return output.sort(caseInsensitiveNameSort)
+	}
 }
