f7759248558df9d7d1d3defbc99593dc7253ca11
eliteSCHW31N <39838619+eliteSchwein@users.noreply.github.com>
2021-01-19 14:00:26 +0100

    fix progress bar in config files panel

--- src/components/panels/Settings/ConfigFilesPanel.vue
+++ src/components/panels/Settings/ConfigFilesPanel.vue
@@ -119,7 +119,7 @@
                 <v-card color="primary">
                     <v-card-text>
                         Please stand by
-                        <v-progress-linear indeterminate color="primary" class="mb-0" ></v-progress-linear>
+                        <v-progress-linear indeterminate color="white" class="mb-0" ></v-progress-linear>
                     </v-card-text>
                 </v-card>
             </v-dialog>
