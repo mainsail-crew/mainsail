d953cab29cab922284fed55da856f9737a8bb120
Eike Ahmels <eike.ahmels@tu-dortmund.de>
2021-02-26 08:52:55 +0100

    new dynamic range on tool slider

--- src/components/panels/MiscellaneousPanel.vue
+++ src/components/panels/MiscellaneousPanel.vue
@@ -17,15 +17,15 @@
 
 <template>
     <div>
-        <v-card class="mt-6" v-if="['printing', 'paused'].includes(printer_state)">
+        <v-card class="mt-6" v-if="['printing', 'paused', 'standby'].includes(printer_state)">
             <v-toolbar flat dense >
                 <v-toolbar-title>
                     <span class="subheading"><v-icon class="mdi mdi-printer-3d" left></v-icon>Print Settings</span>
                 </v-toolbar-title>
             </v-toolbar>
-            <tool-slider label="Speed factor" :target="speed_factor" :max="200" :multi="100" :step="5" command="M220" attribute-name="S" ></tool-slider>
+            <tool-slider label="Speed factor" :target="speed_factor" :max="200" :multi="100" :step="5" :dynamic-range="true" command="M220" attribute-name="S" ></tool-slider>
             <v-divider></v-divider>
-            <tool-slider label="Extrusion factor" :target="extrude_factor" :max="200" :multi="100" :step="1" command="M221" attribute-name="S" ></tool-slider>
+            <tool-slider label="Extrusion factor" :target="extrude_factor" :max="200" :multi="100" :step="1" :dynamic-range="true" command="M221" attribute-name="S" ></tool-slider>
         </v-card>
         <v-card class="mt-6" v-if="this['printer/getMiscellaneous'].length">
             <v-toolbar flat dense >
@@ -66,5 +66,8 @@
                 'printer/getMiscellaneous',
             ]),
         },
+        mounted() {
+            console.log(this.printer_state);
+        }
     }
 </script>
--- src/inputs/ToolSlider.vue
+++ src/inputs/ToolSlider.vue
@@ -10,15 +10,36 @@
             <v-col class="pb-1 pt-3">
                 <v-subheader class="_tool-slider-subheader">
                     <span>{{ label }}</span>
+                    <v-btn
+                        v-if="value !== defaultValue"
+                        class="ml-2"
+                        x-small
+                        icon
+                        @click="processedMin = min; processedMax = max; value = defaultValue; sliding = true; sendCmd()"
+                    >
+                        <v-icon>mdi-restart</v-icon>
+                    </v-btn>
                     <v-spacer></v-spacer>
-                    <span class="font-weight-bold">{{ value }} {{ unit }}</span>
+                    <span class="font-weight-bold">
+                        <v-progress-circular
+                            v-if="sliding && (value === processedMin || value === processedMax)"
+                            class="mr-2"
+                            indeterminate
+                            color="info"
+                            :size="20"
+                            :width="2"
+                        ></v-progress-circular>
+                        {{ value }} {{ unit }}
+                    </span>
                 </v-subheader>
                 <v-card-text class="py-0">
                     <v-slider
                         v-model="value"
-                        :min="min"
-                        :max="max"
-                        @change="sendCmd"
+                        :min="processedMin"
+                        :max="processedMax"
+                        @start="sliding = true"
+                        @end="sendCmd"
+                        @input="processNewRange"
                         hide-details>
 
                         <template v-slot:prepend>
@@ -38,11 +59,6 @@
 
 <script>
     export default {
-        data: function() {
-            return {
-                value: this.target * this.multi,
-            }
-        },
         props: {
             target: {
                 type: Number,
@@ -81,6 +97,16 @@
                 required: false,
                 default: 100
             },
+            dynamicRange: {
+                type: Boolean,
+                required: false,
+                default: false
+            },
+            defaultValue: {
+                type: Number,
+                required: false,
+                default: 100
+            },
             step: {
                 type: Number,
                 required: false,
@@ -92,14 +118,65 @@
                 default: 1
             },
         },
+        data() {
+            return {
+                value: this.target * this.multi,
+                startValue: this.target * this.multi,
+                sliding: false,
+                processedMin: this.min,
+                processedMax: this.max,
+                dynamicStep: Math.floor((this.max - this.min) / 2),
+                processingTimer: null
+            }
+        },
+        created() {
+            if (this.value > this.max || this.value < this.min) {
+                this.processedMax = Math.ceil(this.value / this.defaultValue) * this.defaultValue;
+                this.processedMin = Math.floor(this.value / this.defaultValue) * this.defaultValue - this.defaultValue;
+            }
+        },
         computed: {
 
         },
+        watch: {
+            target: function(newVal) {
+                this.value = newVal * this.multi;
+            },
+        },
         methods: {
+            processNewRange() {
+                if (!this.dynamicRange || !this.sliding) {
+                    return;
+                }
+                if (this.processingTimer) {
+                    clearInterval(this.processingTimer);
+                    this.processingTimer = null;
+                }
+                if (this.value === this.processedMax || this.value === this.processedMin) {
+                    this.processingTimer = setInterval(() => {
+                        let was = '';
+                        if (this.value === this.processedMax) {
+                            was = 'max';
+                        } else if(this.value === this.processedMin) {
+                            was = 'min';
+                        }
+                        this.processedMin = Math.max(0, this.value - this.dynamicStep);
+                        this.processedMax = Math.min(1000, this.value + this.dynamicStep);
+                        this.value = was === 'max' ? this.processedMax : this.processedMin;
+                    }, 500);
+                }
+            },
             sendCmd() {
-                let gcode = this.command+' '+this.attributeName+(this.value*this.attributeScale).toFixed(0)
-                this.$store.commit('server/addEvent', { message: gcode, type: 'command' })
-                this.$socket.sendObj('printer.gcode.script', { script: gcode })
+                if (this.sliding) {
+                    let gcode = this.command + ' ' + this.attributeName + (Math.max(1, this.value) * this.attributeScale).toFixed(0)
+                    this.$store.commit('server/addEvent', {message: gcode, type: 'command'})
+                    this.$socket.sendObj('printer.gcode.script', {script: gcode})
+                    this.sliding = false;
+                    if (this.processingTimer) {
+                        clearInterval(this.processingTimer);
+                        this.processingTimer = null;
+                    }
+                }
             },
             decrement() {
                 this.value = this.value > this.min ? (this.value - this.step).toFixed(0) : this.min
@@ -109,13 +186,6 @@
                 this.value = this.value < this.max ? (this.value + this.step).toFixed(0) : this.max
                 this.sendCmd();
             }
-        },
-        watch: {
-            target: function(newVal) {
-                this.value = newVal * this.multi;
-            },
-        },
-        created: function() {
         }
     }
 </script>
