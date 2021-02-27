5363322681b05b96b44fb2e3eadb111c5a639044
Eike Ahmels <eike.ahmels@tu-dortmund.de>
2021-02-27 18:06:14 +0100

    correct math rounding and clamping

--- src/components/panels/MiscellaneousPanel.vue
+++ src/components/panels/MiscellaneousPanel.vue
@@ -25,7 +25,7 @@
             </v-toolbar>
             <tool-slider label="Speed factor" :target="speed_factor" :max="200" :multi="100" :step="5" :dynamic-range="true" command="M220" attribute-name="S" ></tool-slider>
             <v-divider></v-divider>
-            <tool-slider label="Extrusion factor" new-dynamic :editable="true" :target="extrude_factor" :max="200" :multi="100" :step="1" :dynamic-range="true" command="M221" attribute-name="S" ></tool-slider>
+            <tool-slider label="Extrusion factor" new-dynamic :editable="true" :target="extrude_factor" :max="1000" :multi="100" :step="1" :dynamic-range="true" command="M221" attribute-name="S" ></tool-slider>
         </v-card>
         <v-card class="mt-6" v-if="this['printer/getMiscellaneous'].length">
             <v-toolbar flat dense >
