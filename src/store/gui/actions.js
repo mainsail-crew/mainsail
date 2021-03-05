bdf18c5bcdd0b46de689dc145254afd57c10e2dd
Stefan Dej <meteyou@gmail.com>
2021-03-05 10:36:51 +0100

    first tests with the moonraker database
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

--- src/store/gui/actions.js
+++ src/store/gui/actions.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Vue from "vue";
 
 export default {
 	reset({ commit }) {
@@ -6,25 +7,27 @@ export default {
 	},
 
 	getData({ commit, dispatch }, payload) {
-		commit('setData', payload)
+		commit('setData', payload.value)
+		window.console.log(payload.value)
 
 		if (
-			'state' in payload &&
-			'tempchart' in payload.state &&
-			'datasetSettings' in payload.state.tempchart
+			'tempchart' in payload.value &&
+			'datasetSettings' in payload.value.tempchart
 		) {
-			for (const key of Object.keys(payload.state.tempchart.datasetSettings)) {
+			for (const key of Object.keys(payload.value.tempchart.datasetSettings)) {
 				if (
-					'color' in payload.state.tempchart.datasetSettings[key] &&
-					typeof payload.state.tempchart.datasetSettings[key].color === "object"
+					'color' in payload.value.tempchart.datasetSettings[key] &&
+					typeof payload.value.tempchart.datasetSettings[key].color === "object"
 				) {
-					dispatch('setTempchartDatasetSetting', { name: key, type: 'color', value: payload.state.tempchart.datasetSettings[key].color.hex })
+					dispatch('setTempchartDatasetSetting', { name: key, type: 'color', value: payload.value.tempchart.datasetSettings[key].color.hex })
 				}
 			}
 		}
+
+		dispatch('printer/init', null, { root: true })
 	},
 
-	setSettings({ commit, dispatch }, payload) {
+	setSettings({ commit }, payload) {
 		commit('setSettings', payload)
 
 		if ('tempchart' in payload && 'boolPowerDatasets' in payload.tempchart) {
@@ -32,7 +35,20 @@ export default {
 			else commit('printer/tempHistory/hidePowerDatasets', {}, { root: true })
 		}
 
-		dispatch('upload')
+		Object.keys(payload).forEach(key => {
+			Vue.prototype.$socket.sendObj('server.database.get_item', { namespace: 'mainsail' }, 'gui/updateSettings', { newVal: payload[key], keyName: key }) //, 'server/getMainsailConfig'
+		})
+
+		//dispatch('upload')
+	},
+
+	updateSettings({ commit }, payload) {
+		const keyName = payload.keyName
+		const newState = keyName in payload.value ? Object.assign(payload.value[keyName], payload.newVal) : payload.newVal
+
+		Vue.prototype.$socket.sendObj('server.database.post_item', { namespace: 'mainsail', key: keyName, value: newState })
+
+		commit('void', {}, { root: true })
 	},
 
 	upload({ state, rootState }) {
@@ -63,9 +79,12 @@ export default {
 		dispatch('upload')
 	},
 
-	addPreset({ commit, dispatch }, payload) {
-		commit("addPreset", payload)
-		dispatch("upload")
+	addPreset({ commit }, payload) {
+		//commit("addPreset", payload)
+		//dispatch("upload")
+		window.console.log(payload)
+
+		commit("void", {}, { root: true })
 	},
 
 	updatePreset({ commit, dispatch }, payload) {
