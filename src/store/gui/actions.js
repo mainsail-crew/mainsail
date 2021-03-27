5528988566f8249df9fafb3025e517890319bf5a
eliteSCHW31N <39838619+eliteSchwein@users.noreply.github.com>
2021-03-27 14:36:10 +0100

    Add Multiple Cams (#235)
    * add webcam store methodes
    
    * add experimentell multicam panel
    
    * add dev stuff
    
    * update getter
    
    * add stream not found picture workspace
    
    * update button label
    
    * finish webcam setting panel
    
    * remove remote mode, move navbar switch, add multicam support to webcam panel
    
    * fix package-lock
    
    * update url not working picture workspace
    
    * add error messages, add icon to cams, remove picture workspace
    
    * fix spacing
    
    * fix layouts, fix forgotten boolean for nav, fix cam selection
    
    * fix layout
    
    * add url rule
    
    * fix picture format
    
    * replace index with name, add check if selected cam is still configured
    
    * fix default value
    
    * update database layout
    
    * fix database entries
    
    * fix cam url validation
    
    * try other url check methode
    
    * remove url validation
    
    * add experimentell cam migration
    
    * fix cam migration
    
    Co-authored-by: eliteSCHW31N <business@eliteschw31n.de>
    Co-authored-by: Stefan Dej <meteyou@gmail.com>

--- src/store/gui/actions.js
+++ src/store/gui/actions.js
@@ -7,7 +7,13 @@ export default {
 	},
 
 	getData({ commit, dispatch, rootState }, payload) {
+		let oldwebcamconfig = payload.value.webcam
+
 		commit('setData', payload.value)
+
+		if (typeof (oldwebcamconfig) !== "undefined") {
+			dispatch('convertCamConfig', oldwebcamconfig)
+		}
 
 		if ('tempchart' in payload.value && 'datasetSettings' in payload.value.tempchart) {
 			commit('setTempchartDatasetSettings', payload.value.tempchart.datasetSettings)
@@ -18,6 +24,52 @@ export default {
 		}
 
 		dispatch('printer/init', null, { root: true })
+	},
+
+	convertCamConfig({ dispatch }, payload) {
+
+		if (typeof (payload.configs) !== "undefined") {
+			return
+		}
+
+		let oldcamconfig = {
+			name: "Default",
+			icon: "mdi-webcam",
+			config: {
+				service: "mjpegstreamer",
+				targetFps: 25,
+				url: "/webcam/?action=stream",
+				flipX: false,
+				flipY: false,
+			},
+		}
+
+		let cleanupconfig = {
+			service: undefined,
+			targetFps: undefined,
+			url: undefined,
+			flipX: undefined,
+			flipY: undefined,
+			rotate: undefined,
+			rotateDegrees: undefined,
+			selectedCam: 'Default',
+			bool: payload.bool,
+			configs: [],
+		}
+
+		if (typeof (payload.url) !== "undefined") {
+			oldcamconfig.config.url = payload.url
+		}
+		if (typeof (payload.service) !== "undefined") {
+			oldcamconfig.config.service = payload.service
+		}
+		oldcamconfig.config.targetFps = payload.targetFps
+		oldcamconfig.config.flipX = payload.flipX
+		oldcamconfig.config.flipY = payload.flipY
+		
+		cleanupconfig.configs.push(oldcamconfig)
+
+		dispatch('setSettings', { 'webcam': cleanupconfig })
 	},
 
 	setSettings({ commit }, payload) {
@@ -82,6 +134,30 @@ export default {
 		dispatch('updateSettings', {
 			keyName: 'presets',
 			newVal: state.presets
+		})
+	},
+
+	addWebcam({ commit, dispatch, state }, payload) {
+		commit("addWebcam", payload)
+		dispatch('updateSettings', {
+			keyName: 'webcam.configs',
+			newVal: state.webcam.configs
+		})
+	},
+
+	updateWebcam({ commit, dispatch, state }, payload) {
+		commit("updateWebcam", payload)
+		dispatch('updateSettings', {
+			keyName: 'webcam.configs',
+			newVal: state.webcam.configs
+		})
+	},
+
+	deleteWebcam({ commit, dispatch, state }, payload) {
+		commit("deleteWebcam", payload)
+		dispatch('updateSettings', {
+			keyName: 'webcam.configs',
+			newVal: state.webcam.configs
 		})
 	},
 
