5f40b23fa0ead05783027ab04ab1ce242485a9be
Stefan Dej <meteyou@gmail.com>
2021-04-04 14:30:32 +0200

    remove debug output
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -6,9 +6,6 @@ function loadLocaleMessages() {
 	const locales = require.context('../locales', true, /[A-Za-z0-9-_,\s]+\.json$/i)
 	const messages = {}
 
-	window.console.log("locales")
-	window.console.log(locales)
-
 	locales.keys().forEach(key => {
 		const matched = key.match(/([A-Za-z0-9-_]+)\./i)
 		if (matched && matched.length > 1) {
