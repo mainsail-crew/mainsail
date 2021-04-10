eb6c20295862dac87bb303fc9a92e5a42f592188
Stefan Dej <meteyou@gmail.com>
2021-04-10 14:23:31 +0200

    fix component name output in printer farm grid
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

--- src/store/farm/printer/getters.js
+++ src/store/farm/printer/getters.js
@@ -1,5 +1,5 @@
-import {themeDir} from "@/store/variables";
-
+import { themeDir } from "@/store/variables"
+import { convertName } from "@/plugins/helpers"
 
 export default {
 
@@ -127,7 +127,7 @@ export default {
 				'target' in state.data[key]
 			) {
 				output.push({
-					name: key,
+					name: convertName(key),
 					value: state.data[key].temperature.toFixed(0)+"° / "+state.data[key].target.toFixed(0)+"°",
 				})
 			}
@@ -139,7 +139,7 @@ export default {
 			'target' in state.data.heater_bed
 		) {
 			output.push({
-				name: 'heater_bed',
+				name: convertName('heater_bed'),
 				value: state.data.heater_bed.temperature.toFixed(0)+"° / "+state.data.heater_bed.target.toFixed(0)+"°"
 			})
 		}
@@ -150,14 +150,14 @@ export default {
 			'target' in state.data['temperature_fan chamber']
 		) {
 			output.push({
-				name: 'chamber',
+				name: convertName('chamber'),
 				value: state.data['temperature_fan chamber'].temperature.toFixed(0)+"° / "+state.data['temperature_fan chamber'].target.toFixed(0)+"°"
 			})
 		}
 
 		if ('temperature_sensor chamber' in state.data) {
 			output.push({
-				name: 'chamber',
+				name: convertName('chamber'),
 				value: state.data['temperature_sensor chamber'].temperature.toFixed(0)+"°"
 			})
 		}
