96db215171a9e4adcd08db5beb76e2f21ca1666d
Eike Ahmels <hui_buh-@gmx.net>
2021-04-12 20:11:27 +0200

    fixing a bug for longpress on ios/safari (#261)
    * image fix, grammar fix
    
    * fixed suggestions bug, some cleanup from old editor, svg edge case
    
    * longpress ios fix

--- src/pages/Files.vue
+++ src/pages/Files.vue
@@ -140,8 +140,9 @@
                     </tr>
                 </template>
 
-                <template #item="{ item }">
+                <template #item="{ index, item }">
                     <tr
+                        :key="`${index} ${item.filename}`"
                         v-longpress:600="(e) => showContextMenu(e, item)"
                         @contextmenu="showContextMenu($event, item)"
                         @click="clickRow(item)"
