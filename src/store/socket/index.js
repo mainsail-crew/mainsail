7895910a08e99424df15a578cc032e94e9eb9ae1
Stefan Dej <meteyou@gmail.com>
2021-05-24 22:16:01 +0200

    fix env remote_mode variable
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

--- src/store/socket/index.js
+++ src/store/socket/index.js
@@ -4,9 +4,9 @@ import getters from './getters'
 
 export function getDefaultState() {
 	return {
-		remoteMode: process.env.VUE_APP_REMOTE_MODE === "true" || (document.location.hostname === "my.mainsail.xyz"),
-		hostname: process.env.VUE_APP_HOSTNAME || (process.env.VUE_APP_REMOTE_MODE === "true" || document.location.hostname === "my.mainsail.xyz" ? "" : window.location.hostname),
-		port: process.env.VUE_APP_PORT || (process.env.VUE_APP_REMOTE_MODE === "true" || document.location.hostname === "my.mainsail.xyz" ? 7125 : window.location.port),
+		remoteMode: process.env.VUE_APP_REMOTE_MODE.toLowerCase() === "true" || (document.location.hostname === "my.mainsail.xyz"),
+		hostname: process.env.VUE_APP_HOSTNAME || (process.env.VUE_APP_REMOTE_MODE.toLowerCase() === "true" || document.location.hostname === "my.mainsail.xyz" ? "" : window.location.hostname),
+		port: process.env.VUE_APP_PORT || (process.env.VUE_APP_REMOTE_MODE.toLowerCase() === "true" || document.location.hostname === "my.mainsail.xyz" ? 7125 : window.location.port),
 		protocol: document.location.protocol === 'https:' ? 'wss' : 'ws',
 		reconnectInterval: process.env.VUE_APP_RECONNECT_INTERVAL || 2000,
 		isConnected: false,
