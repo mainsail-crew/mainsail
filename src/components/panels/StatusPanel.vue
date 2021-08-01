c12e9348a0fdfd815290bc58079fdcedb54effee
Stefan Dej <meteyou@gmail.com>
2021-08-01 13:57:13 +0200

    feature: live extruder, toolhead speed and position data in StatusPanel.vue
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

--- src/components/panels/StatusPanel.vue
+++ src/components/panels/StatusPanel.vue
@@ -143,7 +143,7 @@
                     </v-col>
                 </v-row>
             </v-container>
-            <template v-if="['printing', 'paused', 'error', 'standby'].includes(printer_state)">
+            <template v-if="['printing', 'paused', 'error'].includes(printer_state)">
                 <v-divider class="my-0"></v-divider>
                 <v-container class="py-0">
                     <v-row class="text-center py-5" align="center">
@@ -169,15 +169,10 @@
                                 <template v-slot:activator="{ on, attrs }">
                                     <div v-bind="attrs" v-on="on">
                                         <strong>{{ $t("Panels.StatusPanel.Flow") }}</strong><br />
-                                        <template v-if="live_extruder_velocity !== null">
-                                            <span class="d-block text-center text-no-wrap">{{ live_flow+" mm&sup3;/s" }}</span>
-                                        </template>
-                                        <template v-else>
-                                            <span class="d-block text-center text-no-wrap">{{ maxFlow.lastValue ? maxFlow.lastValue.toFixed(1)+" mm&sup3;/s" : "--" }}</span>
-                                        </template>
+                                        <span class="d-block text-center text-no-wrap">{{ live_flow+" mm&sup3;/s" }}</span>
                                     </div>
                                 </template>
-                                <span>{{ $t("Panels.StatusPanel.Max") }}: {{ maxFlow.maxValue ? maxFlow.maxValue.toFixed(1)+" mm&sup3;/s" : "--" }}</span>
+                                <span>{{ $t("Panels.StatusPanel.Max") }}: {{ maxFlow ? maxFlow+" mm&sup3;/s" : "--" }}</span>
                             </v-tooltip>
                         </v-col>
                         <v-col class="col-3 pa-0">
@@ -283,13 +278,7 @@ import VueLoadImage from "vue-load-image"
     VueLoadImage
 })
 export default class StatusPanel extends Mixins(BaseMixin) {
-    maxFlow = {
-        intervalTimer: null,
-        lastExtruderPos: 0,
-        lastTime: 0,
-        lastValue: 0,
-        maxValue: 0,
-    }
+    maxFlow = 0
 
     get current_filename() {
         return this.$store.state.printer.print_stats?.filename ?? ""
@@ -571,49 +560,11 @@ export default class StatusPanel extends Mixins(BaseMixin) {
         return h+":"+m+((diff > 60*60*24*1000) ? "+"+parseInt(diff / (60*60*24*1000)) : "")
     }
 
-    calcMaxFlow() {
-        const newExtruderPos = parseFloat(this.filament_used)
+    @Watch('live_flow')
+    live_flowChanged(newVal) {
+        newVal = parseFloat(newVal)
 
-        if (
-            this.maxFlow.lastExtruderPos &&
-            this.maxFlow.lastExtruderPos < newExtruderPos &&
-            this.maxFlow.lastTime
-        ) {
-            const timeDiff = (new Date().getTime() - this.maxFlow.lastTime) / 1000
-            const filamentDiff = newExtruderPos - this.maxFlow.lastExtruderPos
-            const filamentCrossSection = Math.pow(this.filament_diameter / 2, 2) * Math.PI
-
-            if (timeDiff < 5000) {
-                this.maxFlow.lastValue = filamentCrossSection * filamentDiff / timeDiff
-
-                if (this.maxFlow.maxValue < this.maxFlow.lastValue) this.maxFlow.maxValue = this.maxFlow.lastValue
-            }
-        }
-
-        this.maxFlow.lastExtruderPos = newExtruderPos
-        this.maxFlow.lastTime = new Date().getTime()
-    }
-
-    created() {
-        this.maxFlow.intervalTimer = setInterval(() => {
-            this.calcMaxFlow()
-        }, 3000)
-    }
-
-    beforeDestroy() {
-        if (this.maxFlow.intervalTimer) {
-            clearInterval(this.maxFlow.intervalTimer)
-            this.maxFlow.intervalTimer = null
-        }
-    }
-
-    @Watch('printerState')
-    printerStateChange(newVal) {
-        if (['complete', 'cancel', 'error', 'standby'].includes(newVal)) {
-            this.maxFlow.lastValue = 0
-            this.maxFlow.maxValue = 0
-            this.maxFlow.lastTime = 0
-        }
+        if (newVal && this.maxFlow < newVal) this.maxFlow = newVal
     }
 }
 </script>
