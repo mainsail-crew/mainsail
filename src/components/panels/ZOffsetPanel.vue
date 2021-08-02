94167a05d24c690471548c852ec8536df2ff5721
Stefan Dej <meteyou@gmail.com>
2021-08-02 16:47:48 +0200

    feature: add support for save gcode offset to endstop/probe
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

--- src/components/panels/ZOffsetPanel.vue
+++ src/components/panels/ZOffsetPanel.vue
@@ -3,49 +3,106 @@
 </style>
 
 <template>
-    <v-card v-if="displayPanel" class="mb-6">
-        <v-toolbar flat dense >
-            <v-toolbar-title>
-                <span class="subheading"><v-icon class="mdi mdi-arrow-collapse-vertical" left></v-icon>{{ $t("Panels.ZOffsetPanel.ZOffset") }}</span>
-            </v-toolbar-title>
-        </v-toolbar>
+    <div>
+        <v-card v-if="displayPanel" class="mb-6">
+            <v-toolbar flat dense >
+                <v-toolbar-title>
+                    <span class="subheading"><v-icon class="mdi mdi-arrow-collapse-vertical" left></v-icon>{{ $t("Panels.ZOffsetPanel.ZOffset") }}</span>
+                </v-toolbar-title>
+                <v-spacer></v-spacer>
+                <template v-if="z_gcode_offset !== 0">
+                    <v-btn small class="px-2 minwidth-0" color="grey darken-3" @click="clearZOffset()" :loading="loadings.includes('babySteppingClear')"><v-icon small>mdi-broom</v-icon></v-btn>
+                    <v-menu offset-y left v-if="existZOffsetApplyProbe && existZOffsetApplyEndstop">
+                        <template v-slot:activator="{ on, attrs }">
+                            <v-btn small class="px-2 minwidth-0 ml-3" color="primary" v-bind="attrs" v-on="on">{{ $t("Panels.ZOffsetPanel.Save") }} <v-icon small>mdi-menu-down</v-icon></v-btn>
+                        </template>
+                        <v-list dense class="py-0">
+                            <v-list-item link @click="saveZOffsetToEndstop">
+                                <v-list-item-icon class="mr-0">
+                                    <v-icon small>mdi-electric-switch</v-icon>
+                                </v-list-item-icon>
+                                <v-list-item-content>
+                                    <v-list-item-title>{{ $t("Panels.ZOffsetPanel.ToEndstop") }}</v-list-item-title>
+                                </v-list-item-content>
+                            </v-list-item>
+                            <v-list-item link @click="saveZOffsetToProbe">
+                                <v-list-item-icon class="mr-0">
+                                    <v-icon small>mdi-elevator</v-icon>
+                                </v-list-item-icon>
+                                <v-list-item-content>
+                                    <v-list-item-title>{{ $t("Panels.ZOffsetPanel.ToProbe") }}</v-list-item-title>
+                                </v-list-item-content>
+                            </v-list-item>
+                        </v-list>
+                    </v-menu>
+                    <v-btn small class="px-2 minwidth-0" color="primary ml-3" v-else-if="existZOffsetApplyProbe && !existZOffsetApplyEndstop" @click="saveZOffsetToProbe"><v-icon small class="mr-1">mdi-content-save</v-icon>{{ $t("Panels.ZOffsetPanel.Save") }}</v-btn>
+                    <v-btn small class="px-2 minwidth-0" color="primary ml-3" v-else-if="!existZOffsetApplyProbe && existZOffsetApplyEndstop" @click="saveZOffsetToEndstop"><v-icon small class="mr-1">mdi-content-save</v-icon>{{ $t("Panels.ZOffsetPanel.Save") }}</v-btn>
+                </template>
+            </v-toolbar>
 
-        <v-card-text class="px-0 py-0">
-            <v-container>
-                <v-row class="py-0">
-                    <v-col class="pb-0 text-center">
-                        <p class="mb-0">{{ $t("Panels.ZOffsetPanel.CurrentOffset") }}: {{ homing_origin.length > 1 ? homing_origin[2].toFixed(3) : 0.000 }}mm</p>
-                    </v-col>
-                </v-row>
-                <v-row>
-                    <v-col class="col text-center d-flex flex-column align-center flex-sm-row justify-center">
-                        <v-btn-toggle dense no-gutters class="mx-2 mt-3 mt-sm-0 order-last flex-nowrap order-sm-first" >
-                            <v-btn small @click="sendBabySteppingDownFine()" class="" :loading="loadings.includes('babySteppingDownFine')" ><v-icon small class="mr-2">mdi-arrow-collapse-down</v-icon> -0.01mm</v-btn>
-                            <v-btn small @click="sendBabySteppingDown()" class="" :loading="loadings.includes('babySteppingDown')" >-0.05mm</v-btn>
-                        </v-btn-toggle>
-                        <v-btn-toggle dense no-gutters class="mx-2 order-first flex-nowrap order-sm-last" >
-                            <v-btn small @click="sendBabySteppingUpFine()" class="" :loading="loadings.includes('babySteppingUpFine')" ><v-icon small class="mr-2">mdi-arrow-expand-up</v-icon> +0.01mm</v-btn>
-                            <v-btn small @click="sendBabySteppingUp()" class="" :loading="loadings.includes('babySteppingUp')" >+0.05mm</v-btn>
-                        </v-btn-toggle>
-                    </v-col>
-                </v-row>
-            </v-container>
-        </v-card-text>
-    </v-card>
+            <v-card-text class="px-0 py-0">
+                <v-container>
+                    <v-row class="py-0">
+                        <v-col class="pb-0 text-center">
+                            <p class="mb-0">{{ $t("Panels.ZOffsetPanel.CurrentOffset") }}: {{ z_gcode_offset.toFixed(3) }}mm</p>
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col class="col text-center d-flex flex-column align-center flex-sm-row justify-center">
+                            <v-btn-toggle dense no-gutters class="mx-2 mt-3 mt-sm-0 order-last flex-nowrap order-sm-first" >
+                                <v-btn small @click="sendBabySteppingDownFine()" class="" :loading="loadings.includes('babySteppingDownFine')" ><v-icon small class="mr-2">mdi-arrow-collapse-down</v-icon> -0.01mm</v-btn>
+                                <v-btn small @click="sendBabySteppingDown()" class="" :loading="loadings.includes('babySteppingDown')" >-0.05mm</v-btn>
+                            </v-btn-toggle>
+                            <v-btn-toggle dense no-gutters class="mx-2 order-first flex-nowrap order-sm-last" >
+                                <v-btn small @click="sendBabySteppingUpFine()" class="" :loading="loadings.includes('babySteppingUpFine')" ><v-icon small class="mr-2">mdi-arrow-expand-up</v-icon> +0.01mm</v-btn>
+                                <v-btn small @click="sendBabySteppingUp()" class="" :loading="loadings.includes('babySteppingUp')" >+0.05mm</v-btn>
+                            </v-btn-toggle>
+                        </v-col>
+                    </v-row>
+                </v-container>
+            </v-card-text>
+        </v-card>
+        <v-dialog v-model="saveOffsetDialog" max-width="290">
+            <v-card>
+                <v-toolbar flat dense >
+                    <v-toolbar-title>
+                        <span class="subheading"><v-icon class="mdi mdi-information" left></v-icon>{{ $t("Panels.ZOffsetPanel.SaveInfoHeadline") }}</span>
+                    </v-toolbar-title>
+                </v-toolbar>
+                <v-card-text class="mt-3" v-if="printerIsPrinting">{{ $t("Panels.ZOffsetPanel.SaveInfoDescriptionPrint") }}</v-card-text>
+                <v-card-text class="mt-3" v-else>{{ $t("Panels.ZOffsetPanel.SaveInfoDescription") }}</v-card-text>
+                <v-card-actions v-if="printerIsPrinting">
+                    <v-spacer></v-spacer>
+                    <v-btn text @click="saveOffsetDialog = false">{{ $t("Panels.ZOffsetPanel.Ok") }}</v-btn>
+                </v-card-actions>
+                <v-card-actions v-else>
+                    <v-spacer></v-spacer>
+                    <v-btn color="primary" text @click="saveConfig">{{ $t("Panels.ZOffsetPanel.SAVE_CONFIG") }}</v-btn>
+                    <v-btn text @click="saveOffsetDialog = false">{{ $t("Panels.ZOffsetPanel.Later") }}</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+    </div>
 </template>
 
 <script lang="ts">
 
 import {Component, Mixins} from "vue-property-decorator";
 import BaseMixin from "../mixins/base";
+import {CommandHelp} from "@/store/printer/types";
 
 @Component
 export default class ZOffsetPanel extends Mixins(BaseMixin) {
+    private saveOffsetDialog = false
 
     get displayPanel() {
         if (!this.klippyIsConnected) return false
 
-        return (['printing', 'paused'].includes(this.printer_state)) || (this.displayZOffsetStandby && ['standby', 'complete', 'cancelled'].includes(this.printer_state))
+        if (this.displayZOffsetStandby) return this.klipperReadyForGui
+        else if (this.klipperReadyForGui && this.z_gcode_offset !== 0) return true
+        else if(['printing', 'paused'].includes(this.printer_state)) return true
+
+        return false
     }
 
     get displayZOffsetStandby() {
@@ -56,8 +113,24 @@ export default class ZOffsetPanel extends Mixins(BaseMixin) {
         return this.$store.state.printer?.gcode_move?.homing_origin ?? []
     }
 
+    get z_gcode_offset() {
+        return this.homing_origin.length > 1 ? Math.round(this.homing_origin[2] * 1000) / 1000 : 0
+    }
+
     get homed_axis() {
         return this.$store.state.printer.toolhead.homed_axes ?? ""
+    }
+
+    get helplist() {
+        return this.$store.state.printer.helplist ?? []
+    }
+
+    get existZOffsetApplyProbe() {
+        return this.helplist.findIndex((gcode: CommandHelp) => gcode.commandLow === 'z_offset_apply_probe') !== -1
+    }
+
+    get existZOffsetApplyEndstop() {
+        return this.helplist.findIndex((gcode: CommandHelp) => gcode.commandLow === 'z_offset_apply_endstop') !== -1
     }
 
     sendBabySteppingDownFine() {
@@ -82,6 +155,33 @@ export default class ZOffsetPanel extends Mixins(BaseMixin) {
         const gcode = "SET_GCODE_OFFSET Z_ADJUST=0.05"+(this.homed_axis === "xyz" ? " MOVE=1" : "")
         this.$store.dispatch('server/addEvent', { message: gcode, type: 'command' })
         this.$socket.emit('printer.gcode.script', { script: gcode }, { loading: 'babySteppingUp' })
+    }
+
+    clearZOffset() {
+        const gcode = "SET_GCODE_OFFSET Z=0"+(this.homed_axis === "xyz" ? " MOVE=1" : "")
+        this.$store.dispatch('server/addEvent', { message: gcode, type: 'command' })
+        this.$socket.emit('printer.gcode.script', { script: gcode }, { loading: 'babySteppingClear' })
+    }
+
+    saveZOffsetToEndstop() {
+        const gcode = "Z_OFFSET_APPLY_ENDSTOP"
+        this.$store.dispatch('server/addEvent', { message: gcode, type: 'command' })
+        this.$socket.emit('printer.gcode.script', { script: gcode })
+        this.saveOffsetDialog = true
+    }
+
+    saveZOffsetToProbe() {
+        const gcode = "Z_OFFSET_APPLY_PROBE"
+        this.$store.dispatch('server/addEvent', { message: gcode, type: 'command' })
+        this.$socket.emit('printer.gcode.script', { script: gcode })
+        this.saveOffsetDialog = true
+    }
+
+    saveConfig() {
+        const gcode = "SAVE_CONFIG"
+        this.$store.dispatch('server/addEvent', { message: gcode, type: 'command' })
+        this.$socket.emit('printer.gcode.script', { script: gcode }, { loading: 'topbarSaveConfig' })
+        this.saveOffsetDialog = false
     }
 }
 </script>
