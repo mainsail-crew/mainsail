102d429a400f4e6976c11ee39133695e6db16818
Stefan Dej <meteyou@gmail.com>
2021-08-06 12:00:53 +0200

    feature: disable heightmap panels when klipper is not ready
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

--- src/pages/Heightmap.vue
+++ src/pages/Heightmap.vue
@@ -4,7 +4,7 @@
 
 <template>
     <div>
-        <v-row>
+        <v-row v-if="klipperReadyForGui">
             <v-col class="col-12 col-md-8">
                 <v-card>
                     <v-toolbar flat dense>
@@ -118,6 +118,18 @@
                     </v-card-text>
                 </v-card>
             </v-col>
+        </v-row>
+        <v-row v-else>
+            <v-alert
+                border="right"
+                colored-border
+                type="warning"
+                elevation="2"
+                class="mx-auto mt-6"
+                max-width="500"
+            >
+                {{ $t("Heightmap.ErrorKlipperNotRunning") }}
+            </v-alert>
         </v-row>
         <v-dialog v-model="renameDialog" persistent max-width="600px">
             <v-card>
