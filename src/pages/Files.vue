d819e70a08133ec3f9e7e5251fcd6bae19fa959e
Juan Rosario <pyroclasmx@yahoo.com>
2021-08-28 18:17:51 -0500

    feature: gcodeviewer (#322)
    * Initial implementation of GCode Viewer in Mainsail
    
    * Revert files.vue
    
    * Reupdate files.vue
    
    * Fix router
    
    * Add render quality and force line mode settings
    
    * Cache off canvas
    
    Dynamically create and cache the canvas element so it does not have to reload and to keep current item loaded. Saves time on reload. Will need to test when vue watches are applied.
    
    * Loading progress indicator
    
    * Turn off loading progress after changing render quality.
    
    * Failed render check.
    
    * Added a live tracking button
    
    * Work on settings
    
    * Hide the loading required button earlier
    
    * Added more settings.
    
    * min and max feed coloring settings
    
    * Set ZClipPlane for WebGL1 viewers
    
    * Bed and Delta
    
    * Force line rendering is saved
    
    Addressed an issue where watchers could try to fire on  a viewer that is not initialized
    
    * Move loading progress bar
    
    * Multiple UI Updates
    
    Add Progress Color setting and persistence
    Add z clip control - enabled when not tracking
    Move loading progress bar into canvas area
    
    * Update to package 1.0.6
    
    * Pulled latest
    
    * Update to @sindarius/gcodeviewer 2.0.4
    
    Setting extruder colors directly was removed and instead has been replaced with configuring tools. This opens up the ability to set a nozzle size as well for tool changer type systems that could have multiple nozzle sizes.
    
    * Set tracking position to 0 when starting tracking.
    
    * A timeout is needed to reset the visual
    
    * Created forceRedraw to deal with a timing issue when starting tracking.
    
    * Update to 2.0.6 package

--- src/pages/Files.vue
+++ src/pages/Files.vue
@@ -294,6 +294,10 @@
                 <v-list-item @click="deleteDirectory(contextMenu.item)" v-if="contextMenu.item.isDirectory">
                     <v-icon class="mr-1">mdi-delete</v-icon> {{ $t('Files.Delete')}}
                 </v-list-item>
+				<v-list-item @click="view3D(contextMenu.item)" v-if="!contextMenu.item.isDirectory">
+					<v-icon class="mr-1">mdi-video-3d</v-icon>
+					{{ $t('Files.View3D') }}
+				</v-list-item>
             </v-list>
         </v-menu>
         <v-dialog v-model="dialogCreateDirectory.show" max-width="400">
@@ -1081,5 +1085,9 @@ export default class PageFiles extends Mixins(BaseMixin) {
             if (headerElement) headerElement.visible = false
         })
     }
+
+	view3D(item: FileStateFile) {
+		this.$router.push({path: '/viewer', query: {filename: this.currentPath + '/' + item.filename}});
+	}
 }
 </script>
