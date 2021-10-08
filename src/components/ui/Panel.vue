2d073577b9cb229a0623b57b2d554c12b1fe6d0e
Stefan Dej <meteyou@gmail.com>
2021-10-08 22:06:39 +0200

    feature: add panel component to normalize all panels
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

---
+++ src/components/ui/Panel.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-card class="mb-6">
+        <v-toolbar flat dense >
+            <v-toolbar-title class="d-flex align-center">
+                <v-icon left v-if="icon !== null">{{ icon }}</v-icon>
+                <span class="subheading" v-if="title">{{ title }}</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <slot name="buttons"></slot>
+        </v-toolbar>
+        <slot></slot>
+    </v-card>
+</template>
+
+<script lang="ts">
+
+import Component from 'vue-class-component'
+import {Mixins, Prop} from 'vue-property-decorator'
+import BaseMixin from '@/components/mixins/base'
+
+@Component
+export default class Panel extends Mixins(BaseMixin) {
+    @Prop({ default: null }) readonly icon!: string | null
+    @Prop({ required: true, default: '' }) readonly title!: string
+}
+</script>
