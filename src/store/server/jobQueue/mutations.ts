f60811255f927cc7b1ed89d16bb67a970206950a
Stefan Dej <meteyou@gmail.com>
2021-11-27 14:09:24 +0100

    feat: add moonraker job queue (#433)
    * feat: add moonraker job_queue module and create store module
    
    Signed-off-by: Stefan Dej <meteyou@gmail.com>
    
    * feat: add jobqueue panel in gcode files
    
    Signed-off-by: Stefan Dej <meteyou@gmail.com>
    
    * fix: remove workaround (pause queue at adding the first element)
    
    this function was added in mooonraker
    
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

---
+++ src/store/server/jobQueue/mutations.ts
@@ -0,0 +1,18 @@
+import { getDefaultState } from './index'
+import { MutationTree } from 'vuex'
+import {ServerJobQueueState, ServerJobQueueStateJob} from '@/store/server/jobQueue/types'
+import Vue from 'vue'
+
+export const mutations: MutationTree<ServerJobQueueState> = {
+    reset(state) {
+        Object.assign(state, getDefaultState())
+    },
+
+    setQueuedJobs(state, payload: ServerJobQueueStateJob[]) {
+        Vue.set(state, 'queued_jobs', payload)
+    },
+
+    setQueueState(state, payload: string) {
+        Vue.set(state, 'queue_state', payload)
+    }
+}
