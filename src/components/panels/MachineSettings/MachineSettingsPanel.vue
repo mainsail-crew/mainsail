5919956d815e8d1185d25d0a9c57ca86bcfd055f
th33xitus <th33xitus@googlemail.com>
2021-11-27 11:32:08 +0100

    feat: firmware retraction settings panel on dashboard
    Signed-off-by: Dominik Willner <th33xitus@gmail.com>

--- src/components/panels/MachineSettings/MachineSettingsPanel.vue
+++ src/components/panels/MachineSettings/MachineSettingsPanel.vue
@@ -6,7 +6,22 @@
         :collapsible="true"
         card-class="machine-settings-panel"
     >
-        <motion-settings></motion-settings>
+        <motion-settings v-if="!existsFirmwareRetraction"></motion-settings>
+
+        <div v-if="existsFirmwareRetraction">
+            <sub-panel
+                :title="$t('Panels.MachineSettingsPanel.MotionSettings.Motion')"
+                sub-panel-class="motion-settings-subpanel"
+            >
+                <motion-settings></motion-settings>
+            </sub-panel>
+            <sub-panel
+                :title="$t('Panels.MachineSettingsPanel.FirmwareRetractionSettings.FirmwareRetraction')"
+                sub-panel-class="firmware-retraction-settings-subpanel"
+            >
+                <firmware-retraction-settings></firmware-retraction-settings>
+            </sub-panel>
+        </div>
     </panel>
 </template>
 
@@ -14,12 +29,23 @@
 import { Component, Mixins } from 'vue-property-decorator'
 import BaseMixin from '@/components/mixins/base'
 import Panel from '@/components/ui/Panel.vue'
+import SubPanel from '@/components/ui/SubPanel.vue'
 import MotionSettings from '@/components/panels/MachineSettings/MotionSettings.vue'
+import FirmwareRetractionSettings from '@/components/panels/MachineSettings/FirmwareRetractionSettings.vue'
 
 @Component({
-    components: {Panel, MotionSettings}
+    components: {
+        Panel,
+        SubPanel,
+        MotionSettings,
+        FirmwareRetractionSettings
+    }
 })
 export default class MachineSettingsPanel extends Mixins(BaseMixin) {
+
+    get existsFirmwareRetraction() {
+        return this.$store.state.printer.configfile?.settings?.firmware_retraction ?? false
+    }
 
 }
 </script>
