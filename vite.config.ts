ebb5d5ceae9c170673eb008f4c1b58ecf34e1a8f
pataar <pietering1@gmail.com>
2022-02-07 16:09:10 +0100

    remove obsolete plugin

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,6 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
-        visualizer(),
         vue(),
         loadVersion(),
         Components({
