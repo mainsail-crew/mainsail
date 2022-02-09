0dc1473f6bcb7c89ab7c5c264aa1487e82dd7088
th33xitus <th33xitus@googlemail.com>
2022-02-09 10:33:35 +0100

    fix: make vuetify touch directive work again
    Signed-off-by: Dominik Willner <th33xitus@gmail.com>

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,12 +1,15 @@
 import '@mdi/font/css/materialdesignicons.css'
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
+import {Touch} from 'vuetify/lib/directives'
 
 
-Vue.use(Vuetify)
+Vue.use(Vuetify, {
+    directives: {Touch}
+})
 
 export default new Vuetify({
-    theme: { 
+    theme: {
         dark: true,
         themes: {
             dark: {
