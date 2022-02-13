40e609bb810e8e79e194b469e609eeccfc505849
th33xitus <th33xitus@googlemail.com>
2022-02-13 22:30:57 +0100

    fix: console error regarding touch directive (#633)

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,11 +1,10 @@
 import '@mdi/font/css/materialdesignicons.css'
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
-import {Touch} from 'vuetify/lib/directives'
-
+import { Touch } from 'vuetify/lib/directives'
 
 Vue.use(Vuetify, {
-    directives: {Touch}
+    directives: { Touch },
 })
 
 export default new Vuetify({
