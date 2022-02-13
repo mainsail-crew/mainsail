cfe9445fcc790552e3e5e66a3bf2056f24d5d6a4
th33xitus <th33xitus@googlemail.com>
2022-02-13 19:25:44 +0100

    feat: add input fields to sliders
    Signed-off-by: Dominik Willner <th33xitus@gmail.com>

--- src/components/inputs/ToolSlider.vue
+++ src/components/inputs/ToolSlider.vue
@@ -2,6 +2,21 @@
     ._tool-slider-subheader {
         height: auto;
     }
+
+    ._slider-input {
+        font-size: 0.875rem;
+        max-width: 4.5rem;
+        margin-left: 12px;
+    }
+
+    ._slider-input >>> .v-input__slot {
+        min-height: 1rem !important;
+    }
+
+    ._slider-input >>> .v-text-field__slot input {
+        padding: 4px 0 4px;
+    }
+
 </style>
 
 <template>
@@ -35,11 +50,11 @@
                         <v-icon>mdi-restart</v-icon>
                     </v-btn>
                     <v-spacer></v-spacer>
-                    <span class="font-weight-bold">
+                    <span v-if="!hasInputField" class="font-weight-bold">
                         {{ value }} {{ unit }}
                     </span>
                 </v-subheader>
-                <v-card-text class="py-0">
+                <v-card-text class="py-0 pb-2 d-flex align-center">
                     <v-slider
                         v-model="value"
                         v-touch="{start: resetLockTimer}"
@@ -51,13 +66,27 @@
                         hide-details>
 
                         <template v-slot:prepend>
-                            <v-icon @click="decrement" :disabled="isLocked">mdi-minus</v-icon>
+                            <v-icon @click="decrement" :disabled="isLocked || value <= min">mdi-minus</v-icon>
                         </template>
 
                         <template v-slot:append>
-                            <v-icon @click="increment" :disabled="isLocked">mdi-plus</v-icon>
+                            <v-icon @click="increment" :disabled="isLocked || value >= processedMax">mdi-plus</v-icon>
                         </template>
                     </v-slider>
+                    <v-text-field
+                        v-if="hasInputField"
+                        class="_slider-input"
+                        v-model="numInput"
+                        @blur="numInput = value"
+                        @keyup.enter="submitInput"
+                        :error="(!dynamicRange && numInput > max) || numInput < min"
+                        :suffix="unit"
+                        type="number"
+                        hide-spin-buttons
+                        hide-details
+                        outlined
+                        dense>
+                    </v-text-field>
                 </v-card-text>
             </v-col>
         </v-row>
@@ -74,10 +103,12 @@ import {Debounce} from 'vue-debounce-decorator'
 export default class ToolSlider extends Mixins(BaseMixin) {
     private timeout: number | undefined
     private isLocked = false
-    value = 0
-    startValue = 0
-    processedMax = 100
-    dynamicStep = 50
+
+    private value = 0
+    private numInput = 0
+    private startValue = 0
+    private processedMax = 100
+    private dynamicStep = 50
 
     @Prop({ type: Number, required: true }) declare readonly target: number
     @Prop({ type: String, required: true }) declare readonly command: string
@@ -88,15 +119,16 @@ export default class ToolSlider extends Mixins(BaseMixin) {
     @Prop({ type: Number, default: 1 }) declare readonly attributeScale: number
     @Prop({ type: Number, default: 0 }) declare readonly min: number
     @Prop({ type: Number, default: 100 }) declare readonly max: number
+    @Prop({ type: Boolean, required: false, default: false }) declare readonly hasInputField: boolean
     @Prop({ type: Boolean, default: false }) declare readonly dynamicRange: boolean
     @Prop({ type: Number, default: 100 })  declare readonly defaultValue: number
     @Prop({ type: Number, default: 100 }) declare readonly step: number
     @Prop({ type: Number, default: 1 }) declare readonly multi: number
 
-    created() {
+    created(): void {
         this.value = this.target * this.multi
         this.startValue = this.target * this.multi
-        this.dynamicStep = Math.floor((this.max - this.min) / 2)
+        this.dynamicStep = Math.floor((this.max) / 2)
 
         if (this.value >= this.processedMax) {
             this.processedMax = (Math.ceil(this.value / this.dynamicStep) + 1) * this.dynamicStep
@@ -104,34 +136,34 @@ export default class ToolSlider extends Mixins(BaseMixin) {
     }
 
     @Watch('lockSliders', {immediate: true})
-    lockSlidersChanged(){
+    lockSlidersChanged(): void {
         this.isLocked = this.lockSliders && this.isTouchDevice
     }
 
-    startLockTimer() {
+    startLockTimer(): void {
         let t = this.lockSlidersDelay
         if (!this.isTouchDevice || !this.lockSliders || (t <= 0)) return
         this.timeout = setTimeout(() => this.isLocked = true, t * 1000)
     }
 
-    resetLockTimer() {
+    resetLockTimer(): void {
         clearTimeout(this.timeout)
     }
 
-    get lockSliders() {
+    get lockSliders(): boolean {
         return this.$store.state.gui.uiSettings.lockSlidersOnTouchDevices
     }
 
-    get lockSlidersDelay() {
+    get lockSlidersDelay(): number {
         return this.$store.state.gui.uiSettings.lockSlidersDelay
     }
 
-    get colorBar() {
+    get colorBar(): string {
         return this.max < this.value ? 'warning' : 'primary'
     }
 
     @Debounce(250)
-    changeSlider() {
+    changeSlider(): void {
         this.sendCmd()
 
         if (!this.dynamicRange) return
@@ -140,8 +172,13 @@ export default class ToolSlider extends Mixins(BaseMixin) {
         }
     }
 
+    @Watch('value', { immediate: true })
+    valueChanged(newVal: number): void {
+        this.numInput = newVal
+    }
+
     @Watch('target', { immediate: true })
-    targetChanged(newVal: number) {
+    targetChanged(newVal: number): void {
         this.value = Math.round(newVal * this.multi)
 
         if (!this.dynamicRange) return
@@ -151,11 +188,17 @@ export default class ToolSlider extends Mixins(BaseMixin) {
     }
 
     @Watch('max', { immediate: true })
-    maxChanged(newVal: number) {
+    maxChanged(newVal: number): void {
         this.processedMax = newVal > this.value ? newVal : Math.ceil(this.value / this.dynamicStep) * this.dynamicStep
     }
 
-    resetSlider() {
+    submitInput(): void {
+        if (!this.dynamicRange && this.numInput > this.max) this.value = this.max
+        else this.value = this.numInput
+        this.sendCmd()
+    }
+
+    resetSlider(): void {
         this.value = this.defaultValue
         this.processedMax = this.max
         if (this.value >= this.processedMax) {
@@ -165,20 +208,22 @@ export default class ToolSlider extends Mixins(BaseMixin) {
         this.sendCmd()
     }
 
-    sendCmd() {
-        const gcode = this.command + ' ' + this.attributeName + (Math.max(1, this.value) * this.attributeScale).toFixed(0)
+    sendCmd(): void {
+        const val = (Math.max(1, this.value) * this.attributeScale).toFixed(0)
+        const gcode = `${this.command} ${this.attributeName}${val}`
+
         this.$store.dispatch('server/addEvent', {message: gcode, type: 'command'})
         this.$socket.emit('printer.gcode.script', {script: gcode})
 
         this.startLockTimer()
     }
 
-    decrement() {
+    decrement(): void {
         this.value = this.value > this.min ? Math.round(this.value - this.step) : this.min
         this.sendCmd()
     }
 
-    increment() {
+    increment(): void {
         this.value = this.value < this.processedMax || this.dynamicRange ? Math.round(this.value + this.step) : this.processedMax
         this.sendCmd()
     }
