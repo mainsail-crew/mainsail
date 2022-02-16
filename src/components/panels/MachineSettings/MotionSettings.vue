1077d39e11011913e536c8bb3366481590c5bb38
th33xitus <th33xitus@googlemail.com>
2022-02-16 22:57:06 +0100

    refactor: use new NumberInput in MotionSettings
    Signed-off-by: Dominik Willner <th33xitus@gmail.com>

--- src/components/panels/MachineSettings/MotionSettings.vue
+++ src/components/panels/MachineSettings/MotionSettings.vue
@@ -2,61 +2,73 @@
     <v-card-text>
         <v-row>
             <v-col class="col-12 col-md-6">
-                <motion-settings-input
+                <number-input
                     :label="$t('Panels.MachineSettingsPanel.MotionSettings.Velocity').toString()"
+                    param="velocity"
+                    @submit="sendCmd"
+                    @target-changed="updateValue"
                     :target="current_velocity"
                     :default-value="max_velocity"
+                    :output-error-msg="true"
                     :hasSpinner="true"
                     :spinnerFactor="5"
                     :step="1"
                     :min="1"
                     :max="null"
                     :dec="0"
-                    unit="mm/s"
-                    attribute-name="VELOCITY"></motion-settings-input>
+                    unit="mm/s"></number-input>
             </v-col>
             <v-col class="col-12 col-md-6">
-                <motion-settings-input
+                <number-input
                     :label="$t('Panels.MachineSettingsPanel.MotionSettings.SquareCornerVelocity').toString()"
+                    param="squareCornerVelocity"
+                    @submit="sendCmd"
+                    @target-changed="updateValue"
                     :target="current_square_corner_velocity"
                     :default-value="max_square_corner_velocity"
+                    :output-error-msg="true"
                     :hasSpinner="true"
                     :step="0.1"
                     :min="0.1"
                     :max="null"
                     :dec="1"
-                    unit="mm/s"
-                    attribute-name="SQUARE_CORNER_VELOCITY"></motion-settings-input>
+                    unit="mm/s"></number-input>
             </v-col>
         </v-row>
         <v-row>
             <v-col class="col-12 col-md-6">
-                <motion-settings-input
+                <number-input
                     :label="$t('Panels.MachineSettingsPanel.MotionSettings.Acceleration').toString()"
+                    param="acceleration"
+                    @submit="sendCmd"
+                    @target-changed="updateValue"
                     :target="current_accel"
                     :default-value="max_accel"
+                    :output-error-msg="true"
                     :hasSpinner="true"
                     :spinnerFactor="100"
                     :step="1"
                     :min="1"
                     :max="null"
                     :dec="0"
-                    unit="mm/s²"
-                    attribute-name="ACCEL"></motion-settings-input>
+                    unit="mm/s²"></number-input>
             </v-col>
             <v-col class="col-12 col-md-6">
-                <motion-settings-input
+                <number-input
                     :label="$t('Panels.MachineSettingsPanel.MotionSettings.MaxAccelToDecel').toString()"
+                    param="accelToDecel"
+                    @submit="sendCmd"
+                    @target-changed="updateValue"
                     :target="current_accel_to_decel"
                     :default-value="max_accel_to_decel"
+                    :output-error-msg="true"
                     :hasSpinner="true"
                     :spinnerFactor="100"
                     :step="1"
                     :min="1"
                     :max="null"
                     :dec="0"
-                    unit="mm/s²"
-                    attribute-name="ACCEL_TO_DECEL"></motion-settings-input>
+                    unit="mm/s²"></number-input>
             </v-col>
         </v-row>
     </v-card-text>
@@ -64,28 +76,39 @@
 
 <script lang="ts">
 import { Component, Mixins } from 'vue-property-decorator'
+import { Debounce } from 'vue-debounce-decorator'
 import BaseMixin from '@/components/mixins/base'
 import Panel from '@/components/ui/Panel.vue'
-import MotionSettingsInput from '@/components/inputs/MotionSettingsInput.vue'
+import NumberInput from '@/components/inputs/NumberInput.vue'
 
 @Component({
-    components: { Panel, MotionSettingsInput },
+    components: { Panel, NumberInput },
 })
 export default class MotionSettings extends Mixins(BaseMixin) {
+    private declare velocity: number
+    private declare acceleration: number
+    private declare accelToDecel: number
+    private declare squareCornerVelocity: number
+
     get current_velocity(): number {
-        return Math.trunc(this.$store.state.printer?.toolhead?.max_velocity ?? 300)
+        this.velocity = Math.trunc(this.$store.state.printer?.toolhead?.max_velocity ?? 300)
+        return this.velocity
     }
 
     get current_accel(): number {
-        return Math.trunc(this.$store.state.printer?.toolhead?.max_accel ?? 3000)
+        this.acceleration = Math.trunc(this.$store.state.printer?.toolhead?.max_accel ?? 3000)
+        return this.acceleration
     }
 
     get current_accel_to_decel(): number {
-        return Math.trunc(this.$store.state.printer?.toolhead?.max_accel_to_decel ?? 1500)
+        this.accelToDecel = Math.trunc(this.$store.state.printer?.toolhead?.max_accel_to_decel ?? 1500)
+        return this.accelToDecel
     }
 
     get current_square_corner_velocity(): number {
-        return Math.floor((this.$store.state.printer?.toolhead?.square_corner_velocity ?? 8) * 10) / 10
+        this.squareCornerVelocity =
+            Math.floor((this.$store.state.printer?.toolhead?.square_corner_velocity ?? 8) * 10) / 10
+        return this.squareCornerVelocity
     }
 
     get max_velocity(): number {
@@ -105,6 +128,38 @@ export default class MotionSettings extends Mixins(BaseMixin) {
             Math.floor((this.$store.state.printer?.configfile?.settings?.printer?.square_corner_velocity ?? 8) * 10) /
             10
         )
+    }
+
+    updateValue(param: string, newVal: number) {
+        const params = ['velocity', 'acceleration', 'accelToDecel', 'squareCornerVelocity']
+        if (!params.includes(param)) return
+
+        switch (param) {
+            case 'velocity':
+                this.velocity = newVal
+                break
+            case 'acceleration':
+                this.acceleration = newVal
+                break
+            case 'accelToDecel':
+                this.accelToDecel = newVal
+                break
+            case 'squareCornerVelocity':
+                this.squareCornerVelocity = newVal
+                break
+        }
+    }
+
+    @Debounce(500)
+    sendCmd(): void {
+        let gcode = `SET_VELOCITY_LIMIT`
+        gcode += ` VELOCITY=${this.velocity}`
+        gcode += ` ACCEL=${this.acceleration}`
+        gcode += ` ACCEL_TO_DECEL=${this.accelToDecel}`
+        gcode += ` SQUARE_CORNER_VELOCITY=${this.squareCornerVelocity}`
+
+        this.$store.dispatch('server/addEvent', { message: gcode, type: 'command' })
+        this.$socket.emit('printer.gcode.script', { script: gcode })
     }
 }
 </script>
