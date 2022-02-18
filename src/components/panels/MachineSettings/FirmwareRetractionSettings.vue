81d3ba9a3d4bf2c5c9eae5a69d8669c48597db24
th33xitus <th33xitus@googlemail.com>
2022-02-18 14:30:39 +0100

    refactor: use new NumberInput in PressureAdvance and FirmwareRetraction Settings
    Signed-off-by: Dominik Willner <th33xitus@gmail.com>

--- src/components/panels/MachineSettings/FirmwareRetractionSettings.vue
+++ src/components/panels/MachineSettings/FirmwareRetractionSettings.vue
@@ -2,64 +2,76 @@
     <v-card-text>
         <v-row>
             <v-col class="col-12 col-md-6">
-                <firmware-retraction-settings-input
+                <number-input
                     :label="$t('Panels.MachineSettingsPanel.FirmwareRetractionSettings.RetractLength').toString()"
+                    param="retractLength"
+                    @submit="sendCmd"
+                    @target-changed="updateValue"
                     :target="current_retract_length"
                     :default-value="config_retract_length"
+                    :output-error-msg="true"
                     :hasSpinner="true"
                     :spinnerFactor="10"
                     :step="0.01"
                     :min="0"
                     :max="null"
                     :dec="2"
-                    unit="mm"
-                    attribute-name="RETRACT_LENGTH"></firmware-retraction-settings-input>
+                    unit="mm"></number-input>
             </v-col>
             <v-col class="col-12 col-md-6">
-                <firmware-retraction-settings-input
+                <number-input
                     :label="$t('Panels.MachineSettingsPanel.FirmwareRetractionSettings.RetractSpeed').toString()"
+                    param="retractSpeed"
+                    @submit="sendCmd"
+                    @target-changed="updateValue"
                     :target="current_retract_speed"
                     :default-value="config_retract_speed"
+                    :output-error-msg="true"
                     :hasSpinner="true"
                     :spinnerFactor="5"
                     :step="1"
                     :min="1"
                     :max="null"
                     :dec="0"
-                    unit="mm/s"
-                    attribute-name="RETRACT_SPEED"></firmware-retraction-settings-input>
+                    unit="mm/s"></number-input>
             </v-col>
         </v-row>
         <v-row>
             <v-col class="col-12 col-md-6">
-                <firmware-retraction-settings-input
+                <number-input
                     :label="
                         $t('Panels.MachineSettingsPanel.FirmwareRetractionSettings.UnretractExtraLength').toString()
                     "
+                    param="unretractExtraLength"
+                    @submit="sendCmd"
+                    @target-changed="updateValue"
                     :target="current_unretract_extra_length"
                     :default-value="config_unretract_extra_length"
+                    :output-error-msg="true"
                     :hasSpinner="true"
                     :spinnerFactor="10"
                     :step="0.01"
                     :min="0"
                     :max="null"
                     :dec="2"
-                    unit="mm"
-                    attribute-name="UNRETRACT_EXTRA_LENGTH"></firmware-retraction-settings-input>
+                    unit="mm"></number-input>
             </v-col>
             <v-col class="col-12 col-md-6">
-                <firmware-retraction-settings-input
+                <number-input
                     :label="$t('Panels.MachineSettingsPanel.FirmwareRetractionSettings.UnretractSpeed').toString()"
+                    param="unretractSpeed"
+                    @submit="sendCmd"
+                    @target-changed="updateValue"
                     :target="current_unretract_speed"
                     :default-value="config_unretract_speed"
+                    :output-error-msg="true"
                     :hasSpinner="true"
                     :spinnerFactor="5"
                     :step="1"
                     :min="1"
                     :max="null"
                     :dec="0"
-                    unit="mm/s"
-                    attribute-name="UNRETRACT_SPEED"></firmware-retraction-settings-input>
+                    unit="mm/s"></number-input>
             </v-col>
         </v-row>
     </v-card-text>
@@ -67,28 +79,40 @@
 
 <script lang="ts">
 import { Component, Mixins } from 'vue-property-decorator'
+import { Debounce } from 'vue-debounce-decorator'
 import BaseMixin from '@/components/mixins/base'
 import Panel from '@/components/ui/Panel.vue'
-import FirmwareRetractionSettingsInput from '@/components/inputs/FirmwareRetractionSettingsInput.vue'
+import NumberInput from '@/components/inputs/NumberInput.vue'
 
 @Component({
-    components: { Panel, FirmwareRetractionSettingsInput },
+    components: { Panel, NumberInput },
 })
 export default class FirmwareRetractionSettings extends Mixins(BaseMixin) {
+    private declare retractLength: number
+    private declare retractSpeed: number
+    private declare unretractExtraLength: number
+    private declare unretractSpeed: number
+
     get current_retract_length(): number {
-        return Math.floor((this.$store.state.printer?.firmware_retraction?.retract_length ?? 0) * 100) / 100
+        this.retractLength =
+            Math.floor((this.$store.state.printer?.firmware_retraction?.retract_length ?? 0) * 100) / 100
+        return this.retractLength
     }
 
     get current_retract_speed(): number {
-        return Math.trunc(this.$store.state.printer?.firmware_retraction?.retract_speed ?? 20)
+        this.retractSpeed = Math.trunc(this.$store.state.printer?.firmware_retraction?.retract_speed ?? 20)
+        return this.retractSpeed
     }
 
     get current_unretract_extra_length(): number {
-        return Math.floor((this.$store.state.printer?.firmware_retraction?.unretract_extra_length ?? 0) * 100) / 100
+        this.unretractExtraLength =
+            Math.floor((this.$store.state.printer?.firmware_retraction?.unretract_extra_length ?? 0) * 100) / 100
+        return this.unretractExtraLength
     }
 
     get current_unretract_speed(): number {
-        return Math.trunc(this.$store.state.printer?.firmware_retraction?.unretract_speed ?? 10)
+        this.unretractSpeed = Math.trunc(this.$store.state.printer?.firmware_retraction?.unretract_speed ?? 10)
+        return this.unretractSpeed
     }
 
     get config_retract_length(): number {
@@ -114,6 +138,38 @@ export default class FirmwareRetractionSettings extends Mixins(BaseMixin) {
 
     get config_unretract_speed(): number {
         return Math.trunc(this.$store.state.printer?.configfile?.settings?.firmware_retraction?.unretract_speed ?? 0)
+    }
+
+    updateValue(param: string, newVal: number) {
+        const params = ['retractLength', 'retractSpeed', 'unretractExtraLength', 'unretractSpeed']
+        if (!params.includes(param)) return
+
+        switch (param) {
+            case 'retractLength':
+                this.retractLength = newVal
+                break
+            case 'retractSpeed':
+                this.retractSpeed = newVal
+                break
+            case 'unretractExtraLength':
+                this.unretractExtraLength = newVal
+                break
+            case 'unretractSpeed':
+                this.unretractSpeed = newVal
+                break
+        }
+    }
+
+    @Debounce(500)
+    sendCmd(): void {
+        let gcode = `SET_RETRACTION`
+        gcode += ` RETRACT_LENGTH=${this.retractLength}`
+        gcode += ` RETRACT_SPEED=${this.retractSpeed}`
+        gcode += ` UNRETRACT_EXTRA_LENGTH=${this.unretractExtraLength}`
+        gcode += ` UNRETRACT_SPEED=${this.unretractSpeed}`
+
+        this.$store.dispatch('server/addEvent', { message: gcode, type: 'command' })
+        this.$socket.emit('printer.gcode.script', { script: gcode })
     }
 }
 </script>
--- src/components/panels/MachineSettings/PressureAdvanceSettings.vue
+++ src/components/panels/MachineSettings/PressureAdvanceSettings.vue
@@ -22,33 +22,39 @@
                 </div>
             </v-col>
             <v-col :class="this.all_extruders.length > 1 ? 'col-12 col-md-6 col-xl-4' : 'col-12 col-md-6'">
-                <pressure-advance-settings-input
+                <number-input
                     :label="$t('Panels.MachineSettingsPanel.PressureAdvanceSettings.Advance').toString()"
+                    param="advance"
+                    @submit="sendCmd"
+                    @target-changed="updateValue"
                     :target="current_pressure_advance"
                     :default-value="default_pressure_advance"
                     :extruder="selectedExtruder"
+                    :output-error-msg="true"
                     :hasSpinner="true"
                     :min="0"
                     :max="null"
                     :step="0.001"
                     :dec="3"
-                    unit="mm/s"
-                    attribute-name="ADVANCE"></pressure-advance-settings-input>
+                    unit="mm/s"></number-input>
             </v-col>
             <v-col :class="this.all_extruders.length > 1 ? 'col-12 col-md-6 col-xl-4' : 'col-12 col-md-6'">
-                <pressure-advance-settings-input
+                <number-input
                     :label="$t('Panels.MachineSettingsPanel.PressureAdvanceSettings.SmoothTime').toString()"
+                    param="smoothTime"
+                    @submit="sendCmd"
+                    @target-changed="updateValue"
                     :target="current_smooth_time"
                     :default-value="default_smooth_time"
                     :extruder="selectedExtruder"
+                    :output-error-msg="true"
                     :hasSpinner="true"
                     :min="0"
                     :max="0.2"
                     :step="0.001"
                     :spinnerFactor="10"
                     :dec="3"
-                    unit="s"
-                    attribute-name="SMOOTH_TIME"></pressure-advance-settings-input>
+                    unit="s"></number-input>
             </v-col>
         </v-row>
     </v-card-text>
@@ -58,14 +64,18 @@
 import { Component, Mixins } from 'vue-property-decorator'
 import BaseMixin from '@/components/mixins/base'
 import Panel from '@/components/ui/Panel.vue'
-import PressureAdvanceSettingsInput from '@/components/inputs/PressureAdvanceSettingsInput.vue'
+import NumberInput from '@/components/inputs/NumberInput.vue'
+import { Debounce } from 'vue-debounce-decorator'
 
 @Component({
-    components: { Panel, PressureAdvanceSettingsInput },
+    components: { Panel, NumberInput },
 })
 export default class PressureAdvanceSettings extends Mixins(BaseMixin) {
     private extruders: string[] = []
     private selectedExtruder = ''
+
+    private declare advance: number
+    private declare smoothTime: number
 
     resetToActiveExtruder(): void {
         this.selectedExtruder = this.$store.state.printer.toolhead?.extruder
@@ -86,11 +96,15 @@ export default class PressureAdvanceSettings extends Mixins(BaseMixin) {
     }
 
     get current_pressure_advance(): number {
-        return Math.floor((this.$store.state.printer?.[this.selectedExtruder]?.pressure_advance ?? 0) * 1000) / 1000
+        this.advance =
+            Math.floor((this.$store.state.printer?.[this.selectedExtruder]?.pressure_advance ?? 0) * 1000) / 1000
+        return this.advance
     }
 
     get current_smooth_time(): number {
-        return Math.floor((this.$store.state.printer?.[this.selectedExtruder]?.smooth_time ?? 0.04) * 1000) / 1000
+        this.smoothTime =
+            Math.floor((this.$store.state.printer?.[this.selectedExtruder]?.smooth_time ?? 0.04) * 1000) / 1000
+        return this.smoothTime
     }
 
     get default_pressure_advance(): number {
@@ -108,6 +122,31 @@ export default class PressureAdvanceSettings extends Mixins(BaseMixin) {
                     ?.pressure_advance_smooth_time ?? 0.04) * 1000
             ) / 1000
         )
+    }
+
+    updateValue(param: string, newVal: number) {
+        const params = ['advance', 'smoothTime']
+        if (!params.includes(param)) return
+
+        switch (param) {
+            case 'advance':
+                this.advance = newVal
+                break
+            case 'smoothTime':
+                this.smoothTime = newVal
+                break
+        }
+    }
+
+    @Debounce(500)
+    sendCmd(): void {
+        let gcode = `SET_PRESSURE_ADVANCE`
+        gcode += ` EXTRUDER=${this.selectedExtruder}`
+        gcode += ` ADVANCE=${this.advance}`
+        gcode += ` SMOOTH_TIME=${this.smoothTime}`
+
+        this.$store.dispatch('server/addEvent', { message: gcode, type: 'command' })
+        this.$socket.emit('printer.gcode.script', { script: gcode })
     }
 }
 </script>
