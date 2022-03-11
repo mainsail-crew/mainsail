dc2f512562e5ef3a79471e3f56e6e160fa28a2ce
th33xitus <th33xitus@googlemail.com>
2022-03-11 17:13:58 +0100

    refactor: remove ControlPanelExtruder
    Signed-off-by: Dominik Willner <th33xitus@gmail.com>

--- src/components/panels/ControlPanel.vue
+++ src/components/panels/ControlPanel.vue
@@ -1,37 +1,14 @@
-<style lang="scss" scoped>
-.btnHomeAxis {
-    width: 36px;
-    min-width: 36px !important;
-}
-
-.btnMinWidthAuto {
-    min-width: auto !important;
-}
-
-.steps {
-    width: 100%;
-    > div {
-        width: 100%;
-        display: flex;
-        > button {
-            flex-grow: 1;
-        }
-    }
-}
-</style>
-
 <template>
     <panel
         v-if="klipperReadyForGui && ['standby', 'paused', 'complete', 'cancelled', 'error'].includes(printer_state)"
         :icon="mdiGamepad"
-        :title="$t('Panels.ControlPanel.Headline')"
+        :title="$t('Panels.ControlPanel.Headline').toString()"
         :collapsible="true"
         card-class="control-panel">
         <v-container>
             <control-panel-cross-control v-if="controlStyle === 'cross'"></control-panel-cross-control>
             <control-panel-circle-control v-else-if="controlStyle === 'circle'"></control-panel-circle-control>
             <control-panel-bars-control v-else></control-panel-bars-control>
-            <control-panel-extruder v-if="existsExtruder"></control-panel-extruder>
         </v-container>
     </panel>
 </template>
@@ -39,7 +16,6 @@
 <script lang="ts">
 import { Component, Mixins } from 'vue-property-decorator'
 import BaseMixin from '../mixins/base'
-import ControlPanelExtruder from '@/components/panels/ControlPanelExtruder.vue'
 import ControlPanelCrossControl from '@/components/panels/ControlPanelCrossControl.vue'
 import ControlPanelBarsControl from '@/components/panels/ControlPanelBarsControl.vue'
 import ControlPanelCircleControl from '@/components/panels/ControlPanelCircleControl.vue'
@@ -51,7 +27,6 @@ import { mdiGamepad } from '@mdi/js'
         ControlPanelCircleControl,
         ControlPanelBarsControl,
         ControlPanelCrossControl,
-        ControlPanelExtruder,
     },
 })
 export default class ControlPanel extends Mixins(BaseMixin) {
@@ -59,14 +34,6 @@ export default class ControlPanel extends Mixins(BaseMixin) {
 
     get controlStyle() {
         return this.$store.state.gui.control.style ?? 'bars'
-    }
-
-    get existsExtruder() {
-        return 'extruder' in this.$store.state.printer
-    }
-
-    get boolExtrudePossible() {
-        return this.$store.getters['printer/getExtrudePossible']
     }
 }
 </script>
