a9d4067044c7fa6d17dcc4f17008eff3a4c03bf4
Stefan Dej <meteyou@gmail.com>
2022-05-08 12:58:34 +0200

    style: fix favicon source code
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -246,15 +246,15 @@ export default class App extends Mixins(BaseMixin) {
             } else {
                 const favicon =
                     'data:image/svg+xml;base64,' +
-                    window.btoa(
-                        `<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 599.38 523.11" xml:space="preserve">
-                                <g>
-                                    <path style="fill:${this.logoColor};" d="M382.29,142.98L132.98,522.82L0,522.68L344.3,0l0,0C352.18,49.06,365.2,97.68,382.29,142.98"/>
-                                    <path style="fill:${this.logoColor};" d="M413.28,213.54L208.5,522.92l132.94,0.19l135.03-206.33l0,0C452.69,284.29,431.53,249.77,413.28,213.54 L413.28,213.54"/>
-                                    <path style="fill:${this.logoColor};" d="M599.38,447.69l-49.25,75.42L417,522.82l101.6-153.67l0,0C543.48,397.35,570.49,423.61,599.38,447.69 L599.38,447.69z"/>
-                                </g>
-                            </svg>`
-                    )
+                    window.btoa(`
+                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 599.38 523.11" xml:space="preserve">
+                            <g>
+                                <path style="fill:${this.logoColor};" d="M382.29,142.98L132.98,522.82L0,522.68L344.3,0l0,0C352.18,49.06,365.2,97.68,382.29,142.98"/>
+                                <path style="fill:${this.logoColor};" d="M413.28,213.54L208.5,522.92l132.94,0.19l135.03-206.33l0,0C452.69,284.29,431.53,249.77,413.28,213.54 L413.28,213.54"/>
+                                <path style="fill:${this.logoColor};" d="M599.38,447.69l-49.25,75.42L417,522.82l101.6-153.67l0,0C543.48,397.35,570.49,423.61,599.38,447.69 L599.38,447.69z"/>
+                            </g>
+                        </svg>
+                    `)
 
                 favicon16.href = favicon
                 favicon32.href = favicon
