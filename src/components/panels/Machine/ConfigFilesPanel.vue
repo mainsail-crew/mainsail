530923a5dd14d9598be0d06a80b4d82dc32f88a9
Stefan Dej <meteyou@gmail.com>
2022-05-11 19:11:56 +0200

    feat: add multi select in config files
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

--- src/components/panels/Machine/ConfigFilesPanel.vue
+++ src/components/panels/Machine/ConfigFilesPanel.vue
@@ -26,6 +26,7 @@
                             v-for="button in filteredToolbarButtons"
                             :key="button.loadingName"
                             class="px-2 minwidth-0 ml-3"
+                            :color="button.color"
                             :loading="button.loadingName !== null && loadings.includes(button.loadingName)"
                             @click="button.click">
                             <v-tooltip top>
@@ -106,7 +107,8 @@
                     itemsPerPageOptions: [10, 25, 50, 100, -1],
                 }"
                 mobile-breakpoint="0"
-                item-key="name">
+                item-key="filename"
+                show-select>
                 <template #no-data>
                     <div class="text-center">{{ $t('Machine.ConfigFilesPanel.Empty') }}</div>
                 </template>
@@ -121,7 +123,7 @@
                         <td class="file-list__select-td pr-0">
                             <v-simple-checkbox v-ripple disabled class="pa-0 mr-0"></v-simple-checkbox>
                         </td>
-                        <td class="pr-0 text-center" style="width: 32px">
+                        <td class="px-0 text-center" style="width: 32px">
                             <v-icon>{{ mdiFolderUpload }}</v-icon>
                         </td>
                         <td class=" " colspan="4">..</td>
@@ -149,7 +151,7 @@
                                 class="pa-0 mr-0"
                                 @click.stop="select(!isSelected)"></v-simple-checkbox>
                         </td>
-                        <td class="pr-0 text-center" style="width: 32px">
+                        <td class="px-0 text-center" style="width: 32px">
                             <v-icon v-if="item.isDirectory">{{ mdiFolder }}</v-icon>
                             <v-icon v-if="!item.isDirectory">{{ mdiFile }}</v-icon>
                         </td>
@@ -386,6 +388,32 @@
                 </v-card-actions>
             </panel>
         </v-dialog>
+        <v-dialog v-model="deleteSelectedDialog" max-width="400">
+            <panel
+                :title="$t('Machine.ConfigFilesPanel.Delete')"
+                card-class="gcode-files-delete-selected-dialog"
+                :margin-bottom="false">
+                <template #buttons>
+                    <v-btn icon tile @click="deleteSelectedDialog = false">
+                        <v-icon>{{ mdiCloseThick }}</v-icon>
+                    </v-btn>
+                </template>
+                <v-card-text>
+                    <p class="mb-0">
+                        {{ $t('Machine.ConfigFilesPanel.DeleteSelectedQuestion', { count: selectedFiles.length }) }}
+                    </p>
+                </v-card-text>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="" text @click="deleteSelectedDialog = false">
+                        {{ $t('Machine.ConfigFilesPanel.Cancel') }}
+                    </v-btn>
+                    <v-btn color="error" text @click="deleteSelectedFiles">
+                        {{ $t('Machine.ConfigFilesPanel.Delete') }}
+                    </v-btn>
+                </v-card-actions>
+            </panel>
+        </v-dialog>
         <v-snackbar v-model="uploadSnackbar.status" :timeout="-1" :value="true" fixed right bottom dark>
             <span v-if="uploadSnackbar.max > 1" class="mr-1">
                 ({{ uploadSnackbar.number }}/{{ uploadSnackbar.max }})
@@ -408,7 +436,7 @@
 import { Component, Mixins } from 'vue-property-decorator'
 import BaseMixin from '@/components/mixins/base'
 import { formatDate, formatFilesize, sortFiles } from '@/plugins/helpers'
-import { FileStateFile } from '@/store/files/types'
+import { FileStateFile, FileStateGcodefile } from '@/store/files/types'
 import axios from 'axios'
 import Panel from '@/components/ui/Panel.vue'
 import { hiddenRootDirectories } from '@/store/variables'
@@ -588,6 +616,8 @@ export default class ConfigFilesPanel extends Mixins(BaseMixin) {
         },
     }
 
+    private deleteSelectedDialog = false
+
     get blockFileUpload() {
         return this.$store.state.gui.view.blockFileUpload ?? false
     }
@@ -599,11 +629,23 @@ export default class ConfigFilesPanel extends Mixins(BaseMixin) {
     get toolbarButtons() {
         return [
             {
+                text: this.$t('Machine.ConfigFilesPanel.Delete'),
+                color: 'error',
+                icon: mdiDelete,
+                loadingName: null,
+                onlyWriteable: true,
+                condition: this.selectedFiles.length > 0,
+                click: () => {
+                    this.deleteSelectedDialog = true
+                },
+            },
+            {
                 text: this.$t('Machine.ConfigFilesPanel.UploadFile'),
                 color: 'grey darken-3',
                 icon: mdiFileUpload,
                 loadingName: null,
                 onlyWriteable: true,
+                condition: true,
                 click: this.uploadFileButton,
             },
             {
@@ -612,6 +654,7 @@ export default class ConfigFilesPanel extends Mixins(BaseMixin) {
                 icon: mdiFilePlus,
                 loadingName: null,
                 onlyWriteable: true,
+                condition: true,
                 click: this.createFile,
             },
             {
@@ -620,6 +663,7 @@ export default class ConfigFilesPanel extends Mixins(BaseMixin) {
                 icon: mdiFolderPlus,
                 loadingName: null,
                 onlyWriteable: true,
+                condition: true,
                 click: this.createDirecotry,
             },
             {
@@ -628,9 +672,10 @@ export default class ConfigFilesPanel extends Mixins(BaseMixin) {
                 icon: mdiRefresh,
                 loadingName: null,
                 onlyWriteable: false,
+                condition: true,
                 click: this.refreshFileList,
             },
-        ]
+        ].filter((rule: any) => rule.condition)
     }
 
     get filteredToolbarButtons() {
@@ -675,7 +720,7 @@ export default class ConfigFilesPanel extends Mixins(BaseMixin) {
 
     get headers() {
         return [
-            { text: '', value: '' },
+            { text: '', value: '', sortable: false },
             { text: this.$t('Machine.ConfigFilesPanel.Name'), value: 'filename' },
             { text: this.$t('Machine.ConfigFilesPanel.Filesize'), value: 'size', align: 'right' },
             { text: this.$t('Machine.ConfigFilesPanel.LastModified'), value: 'modified', align: 'right' },
@@ -949,6 +994,27 @@ export default class ConfigFilesPanel extends Mixins(BaseMixin) {
             { path: this.absolutePath + '/' + this.contextMenu.item.filename },
             { action: 'files/getDeleteFile' }
         )
+    }
+
+    deleteSelectedFiles() {
+        this.selectedFiles.forEach((item: FileStateGcodefile) => {
+            if (item.isDirectory) {
+                this.$socket.emit(
+                    'server.files.delete_directory',
+                    { path: this.absolutePath + '/' + item.filename, force: true },
+                    { action: 'files/getDeleteDir' }
+                )
+            } else {
+                this.$socket.emit(
+                    'server.files.delete_file',
+                    { path: this.absolutePath + '/' + item.filename },
+                    { action: 'files/getDeleteFile' }
+                )
+            }
+        })
+
+        this.selectedFiles = []
+        this.deleteSelectedDialog = false
     }
 
     uploadFileButton() {
