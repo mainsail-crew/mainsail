1e4a27426f3c69df06ff7b917813713ae0aedce3
Stefan Dej <meteyou@gmail.com>
2022-05-18 23:56:40 +0200

    fix: add file from sub directory to job queue
    fixes #825
    
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

--- src/components/panels/GcodefilesPanel.vue
+++ src/components/panels/GcodefilesPanel.vue
@@ -1152,9 +1152,8 @@ export default class GcodefilesPanel extends Mixins(BaseMixin) {
     }
 
     addToQueue(item: FileStateGcodefile | FileStateFile) {
-        let path = this.currentPath.slice(7)
-        if (path != '') path += '/'
-        const filename = path + item.filename
+        let filename = [this.currentPath, item.filename].join('/')
+        if (filename.startsWith('/')) filename = filename.slice(1)
 
         this.$store.dispatch('server/jobQueue/addToQueue', [filename])
     }
