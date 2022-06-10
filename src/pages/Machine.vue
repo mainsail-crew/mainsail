e4d28f60d60b9ae2f2bf8c2fbcefea05018ab644
Stefan Dej <meteyou@gmail.com>
2022-06-10 21:05:40 +0200

    fix: logfile panel size and margins
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

--- src/pages/Machine.vue
+++ src/pages/Machine.vue
@@ -9,10 +9,10 @@
                 <system-panel></system-panel>
                 <update-panel></update-panel>
                 <v-row>
-                    <v-col v-if="klipperState === 'ready'" class="col-md-6 pb-0 pb-md-3">
+                    <v-col v-if="klipperState === 'ready'" class="col-12 col-sm-6 pb-0 pb-md-3">
                         <endstop-panel></endstop-panel>
                     </v-col>
-                    <v-col :class="klipperState !== 'ready' ? 'col-md-12' : 'col-md-6 pl-0 pl-md-3'">
+                    <v-col :class="klipperState !== 'ready' ? 'col-12' : 'col-12 col-sm-6 pt-0 pt-sm-3'">
                         <logfiles-panel></logfiles-panel>
                     </v-col>
                 </v-row>
