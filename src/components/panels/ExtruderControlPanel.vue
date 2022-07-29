b0ef87b334af959acd731c8e9d9c0f68826b4a87
th33xitus <th33xitus@googlemail.com>
2022-07-29 21:24:20 +0200

    style: move styles to the end of the file (vue style guide)
    Signed-off-by: Dominik Willner <th33xitus@gmail.com>

--- src/components/panels/ExtruderControlPanel.vue
+++ src/components/panels/ExtruderControlPanel.vue
@@ -1,48 +1,3 @@
-<style lang="scss" scoped>
-._btn-group {
-    border-radius: 4px;
-    display: inline-flex;
-    flex-wrap: nowrap;
-    max-width: 100%;
-    min-width: 100%;
-    width: 100%;
-
-    .v-btn {
-        border-radius: 0;
-        border-color: rgba(255, 255, 255, 0.12);
-        border-style: solid;
-        border-width: thin;
-        box-shadow: none;
-        height: 28px;
-        opacity: 0.8;
-        min-width: auto !important;
-    }
-
-    .v-btn:first-child {
-        border-top-left-radius: inherit;
-        border-bottom-left-radius: inherit;
-    }
-
-    .v-btn:last-child {
-        border-top-right-radius: inherit;
-        border-bottom-right-radius: inherit;
-    }
-
-    .v-btn:not(:first-child) {
-        border-left-width: 0;
-    }
-}
-
-._btn-qs {
-    font-size: 0.8rem !important;
-    max-height: 24px;
-}
-
-._btn-extruder-cmd {
-    min-width: 135px !important;
-}
-</style>
-
 <template>
     <panel
         v-if="klipperReadyForGui && extruders.length"
@@ -573,3 +528,48 @@ export default class ExtruderControlPanel extends Mixins(BaseMixin, ControlMixin
     }
 }
 </script>
+
+<style lang="scss" scoped>
+._btn-group {
+    border-radius: 4px;
+    display: inline-flex;
+    flex-wrap: nowrap;
+    max-width: 100%;
+    min-width: 100%;
+    width: 100%;
+
+    .v-btn {
+        border-radius: 0;
+        border-color: rgba(255, 255, 255, 0.12);
+        border-style: solid;
+        border-width: thin;
+        box-shadow: none;
+        height: 28px;
+        opacity: 0.8;
+        min-width: auto !important;
+    }
+
+    .v-btn:first-child {
+        border-top-left-radius: inherit;
+        border-bottom-left-radius: inherit;
+    }
+
+    .v-btn:last-child {
+        border-top-right-radius: inherit;
+        border-bottom-right-radius: inherit;
+    }
+
+    .v-btn:not(:first-child) {
+        border-left-width: 0;
+    }
+}
+
+._btn-qs {
+    font-size: 0.8rem !important;
+    max-height: 24px;
+}
+
+._btn-extruder-cmd {
+    min-width: 135px !important;
+}
+</style>
