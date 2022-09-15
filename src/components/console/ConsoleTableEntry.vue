6cf0c36234146a7257d2e819f4cafbaa3ffbecfd
A-grid <dev@agrid.studio>
2022-09-15 15:17:50 +0200

    Add colors for 'command' console messages

--- src/components/console/ConsoleTableEntry.vue
+++ src/components/console/ConsoleTableEntry.vue
@@ -50,11 +50,12 @@ export default class ConsoleTableEntry extends Vue {
 
     colorConsoleMessage(item: ServerStateEvent): string {
         if (item.message.match('(^!! )|(^//\\s([Ee]rror//s?:))')) return 'error--text console-message--error'
-        if (item.message.match('^//\\s((Klipper state: Ready)|[Ss]uccess//s?:)|√')) return 'success--text console-message--success'
-        if (item.message.match('^//\\s(Klipper state:)|([Ww]arning//s?:)|(\\(!\\))')) return 'warning--text console-message--warning'
+        if (item.message.match('^//\\s((Klipper state: Ready)|[Ss]uccess//s?:)|√'))
+            return 'success--text console-message--success'
+        if (item.message.match('^//\\s(Klipper state:)|([Ww]arning//s?:)|(\\(!\\))'))
+            return 'warning--text console-message--warning'
         if (item.message.match('^//\\s([Ii]nfo//s?:)|(\\(i\\))')) return 'info--text console-message--info'
         if (item.message.match('^//\\s(\\[(.*?)\\])|@')) return 'accent--text console-message--accent'
-
         return 'text--primary'
     }
 
