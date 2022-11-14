92a65674e8c5ef5577598aee004ac59786ad2ca0
Stefan Dej <meteyou@gmail.com>
2022-11-14 20:52:41 +0100

    refactor: remove debug output
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

--- src/components/TheTopbar.vue
+++ src/components/TheTopbar.vue
@@ -52,7 +52,7 @@
                 :loading="loadings.includes('btnUploadAndStart')"
                 @click="btnUploadAndStart">
                 <v-icon class="mr-md-2">{{ mdiFileUpload }}</v-icon>
-                <span class="d-none d-md-inline">{{ $t('App.TopBar.UploadPrint') }}{{ isIOS ? ' iOS' : '' }}</span>
+                <span class="d-none d-md-inline">{{ $t('App.TopBar.UploadPrint') }}</span>
             </v-btn>
             <v-btn
                 v-if="klippyIsConnected"
