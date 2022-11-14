2aa181c7f567f8bbc9828ea4c921b8dd47711491
Stefan Dej <meteyou@gmail.com>
2022-11-14 21:31:10 +0100

    fix: cannot upload GCODE files on iOS (#1152)
    fix https://github.com/mainsail-crew/mainsail/issues/1117

--- src/components/mixins/base.ts
+++ src/components/mixins/base.ts
@@ -83,6 +83,14 @@ export default class BaseMixin extends Vue {
         return 'ontouchstart' in window || (navigator.maxTouchPoints > 0 && navigator.maxTouchPoints !== 256)
     }
 
+    get isIOS() {
+        return !!(
+            navigator.userAgent.match(/(iPad|iPhone|iPod)/) ||
+            // @ts-ignore
+            (navigator.platform === 'MacIntel' && typeof navigator.standalone !== 'undefined')
+        )
+    }
+
     get moonrakerComponents() {
         return this.$store.state.server?.components ?? []
     }
