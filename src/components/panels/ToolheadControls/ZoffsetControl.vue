e143ac32b0912aa5c7c7d68b81fc3f365261dbfd
milkapot <milkapot@hotmail.com>
2023-01-15 07:49:49 +0100

    Update Zoffset panel to Yoffset panel
    change Z by Y axis

--- src/components/panels/ToolheadControls/ZoffsetControl.vue
+++ src/components/panels/ToolheadControls/ZoffsetControl.vue
@@ -11,12 +11,12 @@
                         <v-icon small class="mr-2">
                             {{ mdiLayersOutline }}
                         </v-icon>
-                        <span>{{ $t('Panels.ZoffsetPanel.Headline') }}: {{ zOffset }}</span>
+                        <span>{{ $t('Panels.YoffsetPanel.Headline') }}: {{ yOffset }}</span>
                     </v-col>
                     <v-col class="v-subheader justify-end pl-0">
                         <div class="d-flex align-center">
                             <v-btn
-                                v-if="z_gcode_offset !== 0"
+                                v-if="y_gcode_offset !== 0"
                                 :loading="loadings.includes('babySteppingClear')"
                                 text
                                 small
@@ -24,29 +24,29 @@
                                 class="px-2 mr-1"
                                 @click="clearZOffset()">
                                 <v-icon small>{{ mdiBroom }}</v-icon>
-                                <span v-if="!el.is.xsmall" class="ml-1">{{ $t('Panels.ZoffsetPanel.Clear') }}</span>
+                                <span v-if="!el.is.xsmall" class="ml-1">{{ $t('Panels.YoffsetPanel.Clear') }}</span>
                             </v-btn>
                             <v-btn
                                 v-if="
-                                    z_gcode_offset !== 0 &&
-                                    ((existZOffsetApplyProbe && !existZOffsetApplyEndstop) ||
-                                        (!existZOffsetApplyProbe && existZOffsetApplyEndstop))
+                                    y_gcode_offset !== 0 &&
+                                    ((existYOffsetApplyProbe && !existYOffsetApplyEndstop) ||
+                                        (!existZOffsetApplyProbe && existYOffsetApplyEndstop))
                                 "
                                 color="primary"
                                 text
                                 small
                                 plain
                                 class="px-2"
-                                @click="saveZOffset">
+                                @click="saveYOffset">
                                 <v-icon small>{{ mdiContentSave }}</v-icon>
-                                <span v-if="!el.is.xsmall" class="ml-1">{{ $t('Panels.ZoffsetPanel.Save') }}</span>
+                                <span v-if="!el.is.xsmall" class="ml-1">{{ $t('Panels.YoffsetPanel.Save') }}</span>
                             </v-btn>
-                            <v-menu v-else-if="z_gcode_offset !== 0" offset-y left :close-on-content-click="false">
+                            <v-menu v-else-if="y_gcode_offset !== 0" offset-y left :close-on-content-click="false">
                                 <template #activator="{ on, attrs }">
                                     <v-btn plain text small color="primary" v-bind="attrs" class="pl-2 pr-0" v-on="on">
                                         <v-icon small>{{ mdiContentSave }}</v-icon>
                                         <span v-if="!el.is.xsmall" class="ml-1">
-                                            {{ $t('Panels.ZoffsetPanel.Save') }}
+                                            {{ $t('Panels.YoffsetPanel.Save') }}
                                         </span>
                                         <v-icon small>{{ mdiMenuDown }}</v-icon>
                                     </v-btn>
@@ -55,13 +55,13 @@
                                     <v-list-item>
                                         <v-btn small style="width: 100%" @click="saveZOffsetToEndstop">
                                             <v-icon left small>{{ mdiElectricSwitch }}</v-icon>
-                                            {{ $t('Panels.ZoffsetPanel.ToEndstop') }}
+                                            {{ $t('Panels.YoffsetPanel.ToEndstop') }}
                                         </v-btn>
                                     </v-list-item>
                                     <v-list-item>
-                                        <v-btn small style="width: 100%" @click="saveZOffsetToProbe">
+                                        <v-btn small style="width: 100%" @click="saveYOffsetToProbe">
                                             <v-icon left small>{{ mdiElevator }}</v-icon>
-                                            {{ $t('Panels.ZoffsetPanel.ToProbe') }}
+                                            {{ $t('Panels.YoffsetPanel.ToProbe') }}
                                         </v-btn>
                                     </v-list-item>
                                 </v-list>
@@ -74,7 +74,7 @@
                         <div class="d-flex align-center">
                             <v-item-group class="_btn-group">
                                 <v-btn
-                                    v-for="(offset, index) in offsetsZ"
+                                    v-for="(offset, index) in offsetsy"
                                     :key="`offsetsUp-${index}`"
                                     small
                                     class="_btn-qs flex-grow-1 px-1"
@@ -90,14 +90,14 @@
                     <v-col :class="!el.is.medium ? 'order-0 col-6' : 'col-12'">
                         <v-item-group v-if="!el.is.medium" class="_btn-group">
                             <v-btn
-                                v-for="(offset, index) in offsetsZ.slice().reverse()"
+                                v-for="(offset, index) in offsetsY.slice().reverse()"
                                 :key="`offsetsDown-${index}`"
                                 small
                                 class="_btn-qs flex-grow-1 px-1"
                                 @click="sendBabyStepDown(offset)">
                                 <span>&minus;{{ offset }}</span>
                                 <v-icon
-                                    v-if="index === offsetsZ.length - 1 && !el.is.xsmall"
+                                    v-if="index === offsetsY.length - 1 && !el.is.xsmall"
                                     left
                                     small
                                     class="mr-n1 ml-1">
@@ -107,7 +107,7 @@
                         </v-item-group>
                         <v-item-group v-else class="_btn-group">
                             <v-btn
-                                v-for="(offset, index) in offsetsZ"
+                                v-for="(offset, index) in offsetsY"
                                 :key="`offsetsDown-${index}`"
                                 small
                                 class="_btn-qs flex-grow-1 px-1"
@@ -123,26 +123,26 @@
 
                 <v-dialog v-model="saveOffsetDialog" max-width="290">
                     <panel
-                        :title="$t('Panels.ZoffsetPanel.SaveInfoHeadline').toString()"
+                        :title="$t('Panels.YoffsetPanel.SaveInfoHeadline').toString()"
                         :icon="mdiInformation"
-                        card-class="zoffset-saveinfo-dialog"
+                        card-class="yoffset-saveinfo-dialog"
                         :margin-bottom="false">
                         <v-card-text v-if="printerIsPrinting" class="mt-3">
-                            {{ $t('Panels.ZoffsetPanel.SaveInfoDescriptionPrint') }}
+                            {{ $t('Panels.YoffsetPanel.SaveInfoDescriptionPrint') }}
                         </v-card-text>
                         <v-card-text v-else class="mt-3">
-                            {{ $t('Panels.ZoffsetPanel.SaveInfoDescription') }}
+                            {{ $t('Panels.YoffsetPanel.SaveInfoDescription') }}
                         </v-card-text>
                         <v-card-actions v-if="printerIsPrinting">
                             <v-spacer></v-spacer>
-                            <v-btn text @click="saveOffsetDialog = false">{{ $t('Panels.ZoffsetPanel.Ok') }}</v-btn>
+                            <v-btn text @click="saveOffsetDialog = false">{{ $t('Panels.YoffsetPanel.Ok') }}</v-btn>
                         </v-card-actions>
                         <v-card-actions v-else>
                             <v-spacer></v-spacer>
                             <v-btn color="primary" text @click="saveConfig">
-                                {{ $t('Panels.ZoffsetPanel.SaveConfig') }}
+                                {{ $t('Panels.YoffsetPanel.SaveConfig') }}
                             </v-btn>
-                            <v-btn text @click="saveOffsetDialog = false">{{ $t('Panels.ZoffsetPanel.Later') }}</v-btn>
+                            <v-btn text @click="saveOffsetDialog = false">{{ $t('Panels.YoffsetPanel.Later') }}</v-btn>
                         </v-card-actions>
                     </panel>
                 </v-dialog>
@@ -189,12 +189,12 @@ export default class ZoffsetControl extends Mixins(BaseMixin) {
         return this.$store.state.printer?.gcode_move?.homing_origin ?? []
     }
 
-    get z_gcode_offset() {
+    get y_gcode_offset() {
         return this.homing_origin.length > 1 ? Math.round(this.homing_origin[2] * 1000) / 1000 : 0
     }
 
-    get offsetsZ() {
-        return this.$store.state.gui.control.offsetsZ
+    get offsetsY() {
+        return this.$store.state.gui.control.offsetsY
     }
 
     get homed_axis() {
@@ -205,54 +205,54 @@ export default class ZoffsetControl extends Mixins(BaseMixin) {
         return this.$store.state.printer.helplist ?? []
     }
 
-    get existZOffsetApplyProbe() {
-        return this.helplist.findIndex((gcode: CommandHelp) => gcode.commandLow === 'z_offset_apply_probe') !== -1
+    get existYOffsetApplyProbe() {
+        return this.helplist.findIndex((gcode: CommandHelp) => gcode.commandLow === 'y_offset_apply_probe') !== -1
     }
 
-    get existZOffsetApplyEndstop() {
-        return this.helplist.findIndex((gcode: CommandHelp) => gcode.commandLow === 'z_offset_apply_endstop') !== -1
+    get existYOffsetApplyEndstop() {
+        return this.helplist.findIndex((gcode: CommandHelp) => gcode.commandLow === 'y_offset_apply_endstop') !== -1
     }
 
-    get zOffset(): number {
+    get yOffset(): number {
         return this.$store.state.printer?.gcode_move?.homing_origin[2].toFixed(3)
     }
 
     sendBabyStepDown(offset: number): void {
-        const gcode = `SET_GCODE_OFFSET Z_ADJUST=-${offset} ${this.homed_axis === 'xyz' ? 'MOVE=1' : ''}`
+        const gcode = `SET_GCODE_OFFSET Y_ADJUST=-${offset} ${this.homed_axis === 'xyz' ? 'MOVE=1' : ''}`
         this.$store.dispatch('server/addEvent', { message: gcode, type: 'command' })
         this.$socket.emit('printer.gcode.script', { script: gcode }, { loading: 'babyStepDown' })
     }
 
     sendBabyStepUp(offset: number): void {
-        const gcode = `SET_GCODE_OFFSET Z_ADJUST=+${offset} ${this.homed_axis === 'xyz' ? 'MOVE=1' : ''}`
+        const gcode = `SET_GCODE_OFFSET Y_ADJUST=+${offset} ${this.homed_axis === 'xyz' ? 'MOVE=1' : ''}`
         this.$store.dispatch('server/addEvent', { message: gcode, type: 'command' })
         this.$socket.emit('printer.gcode.script', { script: gcode }, { loading: 'babyStepUp' })
     }
 
-    clearZOffset(): void {
-        const gcode = 'SET_GCODE_OFFSET Z=0' + (this.homed_axis === 'xyz' ? ' MOVE=1' : '')
+    clearYOffset(): void {
+        const gcode = 'SET_GCODE_OFFSET Y=0' + (this.homed_axis === 'xyz' ? ' MOVE=1' : '')
         this.$store.dispatch('server/addEvent', { message: gcode, type: 'command' })
         this.$socket.emit('printer.gcode.script', { script: gcode }, { loading: 'babySteppingClear' })
     }
 
-    saveZOffset(): void {
-        if (this.existZOffsetApplyProbe && !this.existZOffsetApplyEndstop) {
-            this.saveZOffsetToProbe()
+    saveYOffset(): void {
+        if (this.existYOffsetApplyProbe && !this.existYOffsetApplyEndstop) {
+            this.saveYOffsetToProbe()
         }
-        if (!this.existZOffsetApplyProbe && this.existZOffsetApplyEndstop) {
-            this.saveZOffsetToEndstop()
+        if (!this.existYOffsetApplyProbe && this.existYOffsetApplyEndstop) {
+            this.saveYOffsetToEndstop()
         }
     }
 
-    saveZOffsetToEndstop(): void {
-        const gcode = 'Z_OFFSET_APPLY_ENDSTOP'
+    saveYOffsetToEndstop(): void {
+        const gcode = 'Y_OFFSET_APPLY_ENDSTOP'
         this.$store.dispatch('server/addEvent', { message: gcode, type: 'command' })
         this.$socket.emit('printer.gcode.script', { script: gcode })
         this.saveOffsetDialog = true
     }
 
-    saveZOffsetToProbe(): void {
-        const gcode = 'Z_OFFSET_APPLY_PROBE'
+    saveYOffsetToProbe(): void {
+        const gcode = 'Y_OFFSET_APPLY_PROBE'
         this.$store.dispatch('server/addEvent', { message: gcode, type: 'command' })
         this.$socket.emit('printer.gcode.script', { script: gcode })
         this.saveOffsetDialog = true
