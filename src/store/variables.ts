920586ef65bb4b78849fe55af00ef5be1417fe49
Stefan Dej <meteyou@gmail.com>
2023-01-29 16:10:53 +0100

    refactor: update moonraker min version
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

--- src/store/variables.ts
+++ src/store/variables.ts
@@ -2,7 +2,7 @@ export const defaultLogoColor = '#D41216'
 export const defaultPrimaryColor = '#2196f3'
 
 export const minKlipperVersion = 'v0.10.0-271'
-export const minMoonrakerVersion = 'v0.7.1-486'
+export const minMoonrakerVersion = 'v0.7.1-797'
 
 export const colorArray = ['#F44336', '#8e379d', '#03DAC5', '#3F51B5', '#ffde03', '#009688', '#E91E63']
 
