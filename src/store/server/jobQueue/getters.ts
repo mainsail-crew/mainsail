1e45c5988995478798fcf61dcacd23c2d8078e00
Stefan Dej <meteyou@gmail.com>
2023-02-11 12:23:09 +0100

    feat: combine multiple same jobs in job queue
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

--- src/store/server/jobQueue/getters.ts
+++ src/store/server/jobQueue/getters.ts
@@ -10,10 +10,17 @@ export const getters: GetterTree<ServerJobQueueState, any> = {
 
         state.queued_jobs.forEach((queuedJob) => {
             const job = { ...queuedJob }
+
+            if (jobs.length && jobs[jobs.length - 1].filename === job.filename) {
+                jobs[jobs.length - 1].count = (jobs[jobs.length - 1].count ?? 1) + 1
+                return
+            }
+
             const file = rootGetters['files/getFile']('gcodes/' + job.filename)
             if (!file?.metadataPulled)
                 Vue.$socket.emit('server.files.metadata', { filename: job.filename }, { action: 'files/getMetadata' })
-            job['metadata'] = file
+            job.metadata = file
+            job.count = 1
 
             jobs.push(job)
         })
