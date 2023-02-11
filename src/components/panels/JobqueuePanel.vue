a669db468ab22eaceea16b7e274a6e511b004a56
Stefan Dej <meteyou@gmail.com>
2023-02-11 12:29:57 +0100

    refactor: thumbnail vue-load-image slots in gcode jobqueuepanel
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

--- src/components/panels/JobqueuePanel.vue
+++ src/components/panels/JobqueuePanel.vue
@@ -68,11 +68,12 @@
                                                 height="32"
                                                 v-bind="attrs"
                                                 v-on="on" />
-                                            <v-progress-circular
-                                                slot="preloader"
-                                                indeterminate
-                                                color="primary"></v-progress-circular>
-                                            <v-icon slot="error">{{ mdiFile }}</v-icon>
+                                            <div slot="preloader">
+                                                <v-progress-circular indeterminate color="primary" />
+                                            </div>
+                                            <div slot="error">
+                                                <v-icon>{{ mdiFile }}</v-icon>
+                                            </div>
                                         </vue-load-image>
                                     </template>
                                     <span><img :src="getBigThumbnail(item)" width="250" /></span>
@@ -81,11 +82,12 @@
                             <template v-else-if="getSmallThumbnail(item)">
                                 <vue-load-image>
                                     <img slot="image" :src="getSmallThumbnail(item)" width="32" height="32" />
-                                    <v-progress-circular
-                                        slot="preloader"
-                                        indeterminate
-                                        color="primary"></v-progress-circular>
-                                    <v-icon slot="error">{{ mdiFile }}</v-icon>
+                                    <div slot="preloader">
+                                        <v-progress-circular indeterminate color="primary" />
+                                    </div>
+                                    <div slot="error">
+                                        <v-icon>{{ mdiFile }}</v-icon>
+                                    </div>
                                 </vue-load-image>
                             </template>
                             <template v-else>
