1fa03e815f72c26e966321c6d97a3fff3a9ee95a
Stefan Dej <meteyou@gmail.com>
2023-04-04 22:54:11 +0200

    feat: add new theme
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

--- src/components/inputs/Codemirror.vue
+++ src/components/inputs/Codemirror.vue
@@ -12,7 +12,7 @@ import BaseMixin from '../mixins/base'
 import { basicSetup } from 'codemirror'
 import { EditorView, keymap } from '@codemirror/view'
 import { EditorState } from '@codemirror/state'
-import { mainsailTheme } from '@/plugins/codemirrorTheme'
+import { vscodeDark } from '@uiw/codemirror-theme-vscode'
 import { StreamLanguage } from '@codemirror/language'
 import { klipper_config } from '@/plugins/StreamParserKlipperConfig'
 import { gcode } from '@/plugins/StreamParserGcode'
@@ -81,8 +81,9 @@ export default class Codemirror extends Mixins(BaseMixin) {
 
     get cmExtensions() {
         const extensions = [
+            EditorView.theme({}, { dark: true }),
             basicSetup,
-            mainsailTheme,
+            vscodeDark,
             keymap.of([indentWithTab]),
             EditorView.updateListener.of((update) => {
                 this.content = update.state?.doc.toString()
