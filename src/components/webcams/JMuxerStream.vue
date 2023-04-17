ee84819a438cc0b98b9a9e139d3d65c610083d58
Christian Iversen <ci@iversenit.dk>
2023-04-18 01:00:42 +0200

    [jmuxer-stream] Fix formatting
    Signed-off-by: Christian Iversen <ci@iversenit.dk>

--- src/components/webcams/JMuxerStream.vue
+++ src/components/webcams/JMuxerStream.vue
@@ -1,15 +1,15 @@
 <style scoped>
- .webcamImage {
-     width: 100%;
- }
+.webcamImage {
+    width: 100%;
+}
 </style>
 
 <template>
-  <video ref="video" v-observe-visibility="visibilityChanged" autoplay :style="webcamStyle" class="webcamImage" />
+    <video ref="video" v-observe-visibility="visibilityChanged" autoplay :style="webcamStyle" class="webcamImage" />
 </template>
 
 <script lang="ts">
-import JMuxer from "jmuxer"
+import JMuxer from 'jmuxer'
 import { Component, Mixins, Prop } from 'vue-property-decorator'
 import BaseMixin from '@/components/mixins/base'
 
@@ -61,33 +61,32 @@ export default class JMuxerStreamer extends Mixins(BaseMixin) {
         const video = this.$refs.video
         const targetFps = this.camSettings.targetFps || 10
 
-        var jmuxer = this.jmuxer = new JMuxer({
+        var jmuxer = (this.jmuxer = new JMuxer({
             node: video,
             mode: 'video',
             flushingTime: 0,
             fps: targetFps,
             // debug: true,
-            onReady: function(data) {
-                console.log("jmuxer ready:", data);
+            onReady: function (data) {
+                console.log('jmuxer ready:', data)
             },
-            onError: function(data) {
-                console.log("jmuxer error:", data);
-            }
-        })
+            onError: function (data) {
+                console.log('jmuxer error:', data)
+            },
+        }))
         var url = this.url
 
-        if (url.startsWith("ws://") || url.startsWith("wss://")) {
-            var ws = new WebSocket(url);
-            ws.binaryType = 'arraybuffer';
-            ws.addEventListener('message', function(event) {
+        if (url.startsWith('ws://') || url.startsWith('wss://')) {
+            var ws = new WebSocket(url)
+            ws.binaryType = 'arraybuffer'
+            ws.addEventListener('message', function (event) {
                 jmuxer.feed({
-                    video: new Uint8Array(event.data)
-                });
-            });
+                    video: new Uint8Array(event.data),
+                })
+            })
         } else {
-            console.error("jmuxer error: only websocket streams supported (ws://.. or wss://..)");
+            console.error('jmuxer error: only websocket streams supported (ws://.. or wss://..)')
         }
-
     }
 
     beforeUnmount() {
