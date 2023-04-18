9ff08f323bef05ee9f97a2b2efe3d4afd85d353e
Stefan Dej <meteyou@gmail.com>
2023-04-18 22:44:21 +0200

    feat: add status output
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

--- src/components/webcams/JMuxerStream.vue
+++ src/components/webcams/JMuxerStream.vue
@@ -1,5 +1,13 @@
 <template>
-    <video ref="video" autoplay :style="webcamStyle" class="webcamImage" />
+    <div>
+        <video ref="video" autoplay :style="webcamStyle" class="webcamImage" />
+        <v-row v-if="status !== 'connected'">
+            <v-col class="_webcam_jmuxer_output text-center d-flex flex-column justify-center align-center">
+                <v-progress-circular v-if="status === 'connecting'" indeterminate color="primary" class="mb-3" />
+                <span class="mt-3">{{ status }}</span>
+            </v-col>
+        </v-row>
+    </div>
 </template>
 
 <script lang="ts">
@@ -10,6 +18,7 @@ import BaseMixin from '@/components/mixins/base'
 @Component
 export default class JMuxerStreamer extends Mixins(BaseMixin) {
     private jmuxer: JMuxer | null = null
+    private status: string = 'connecting'
 
     @Prop({ required: true })
     camSettings: any
@@ -43,11 +52,13 @@ export default class JMuxerStreamer extends Mixins(BaseMixin) {
     }
 
     play() {
+        this.status = 'connecting'
         this.jmuxer?.destroy()
 
         // Only websocket streams supported
         if (!this.url.startsWith('ws://') && !this.url.startsWith('wss://')) {
             console.error('jmuxer error: only websocket streams supported (ws://.. or wss://..)')
+            this.status = 'error'
             return
         }
 
@@ -61,9 +72,11 @@ export default class JMuxerStreamer extends Mixins(BaseMixin) {
             fps: targetFps,
             // debug: true,
             onReady: (data: any) => {
+                this.status = 'connected'
                 console.log('jmuxer ready:', data)
             },
             onError: (data: any) => {
+                this.status = 'error'
                 console.log('jmuxer error:', data)
             },
         })
@@ -86,5 +99,9 @@ export default class JMuxerStreamer extends Mixins(BaseMixin) {
 <style scoped>
 .webcamImage {
     width: 100%;
+}
+
+._webcam_jmuxer_output {
+    aspect-ratio: calc(3 / 2);
 }
 </style>
