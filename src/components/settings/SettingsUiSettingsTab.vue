335abb705ef3473062101e22fdb1aecab117facb
Mike Constantino <mconstantino@digitalocean.com>
2023-05-07 15:37:33 -0400

    Update SettingsUiSettingsTab.vue

--- src/components/settings/SettingsUiSettingsTab.vue
+++ src/components/settings/SettingsUiSettingsTab.vue
@@ -341,7 +341,7 @@ export default class SettingsUiSettingsTab extends Mixins(BaseMixin) {
     }
 
     get disableFanAnimation() {
-        return this.$store.state.gui.uiSettings.disableFanAnimation ?? false
+        return this.$store.state.gui.uiSettings.hideSaveConfigForBedMash ?? false
     }
 
     set disableFanAnimation(newVal) {
