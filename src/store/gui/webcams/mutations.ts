4cd54151a6b4e34f98726dfbaa97ce5efb782430
Stefan Dej <meteyou@gmail.com>
2023-06-28 20:32:49 +0200

    refactor: The unused import `GuiWebcamStateWebcam` was removed from `src/store/gui/webcams/mutations.ts`
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

--- src/store/gui/webcams/mutations.ts
+++ src/store/gui/webcams/mutations.ts
@@ -1,6 +1,6 @@
 import { getDefaultState } from './index'
 import { MutationTree } from 'vuex'
-import { GuiWebcamState, GuiWebcamStateWebcam } from '@/store/gui/webcams/types'
+import { GuiWebcamState } from '@/store/gui/webcams/types'
 import Vue from 'vue'
 
 export const mutations: MutationTree<GuiWebcamState> = {
