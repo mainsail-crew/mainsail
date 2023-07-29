032e8bb7ee14cd618d835f7b47fb93d9623aa5ea
Stefan Dej <meteyou@gmail.com>
2023-07-29 23:57:38 +0200

    fix: fix issue with create/edit presets and refactor SettingsPresetsTab.vue
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

---
+++ src/components/settings/Presets/PresetsEntryCooldown.vue
@@ -0,0 +1,26 @@
+<template>
+    <settings-row :title="$t('Settings.PresetsTab.Cooldown')">
+        <v-btn small outlined class="ml-3" @click="editCooldown">
+            <v-icon left small>{{ mdiPencil }}</v-icon>
+            {{ $t('Settings.Edit') }}
+        </v-btn>
+    </settings-row>
+</template>
+
+<script lang="ts">
+import { Component, Mixins } from 'vue-property-decorator'
+import BaseMixin from '@/components/mixins/base'
+import SettingsRow from '@/components/settings/SettingsRow.vue'
+import { mdiPencil } from '@mdi/js'
+
+@Component({
+    components: { SettingsRow },
+})
+export default class PresetsEntryCooldown extends Mixins(BaseMixin) {
+    mdiPencil = mdiPencil
+
+    editCooldown() {
+        this.$emit('edit')
+    }
+}
+</script>
