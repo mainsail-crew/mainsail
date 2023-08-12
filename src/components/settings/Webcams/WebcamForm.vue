3188cc2f798bf6993cfd4963bcebacb4d65b47e7
HelgeKeck <helgekeck@hotmail.com>
2023-08-12 18:04:10 +0200

    fixes

--- src/components/settings/Webcams/WebcamForm.vue
+++ src/components/settings/Webcams/WebcamForm.vue
@@ -134,14 +134,14 @@
                                 :label="$t('Settings.WebcamsTab.Vertically')" />
                         </v-col>
                     </v-row>
-                    <v-row class="mt-5">
+                    <v-row class="mt-10">
                         <v-col class="pt-1 pb-3">
                             <div class="v-label v-label--active theme--dark text-subtitle-1">
                                 {{ $t('Settings.WebcamsTab.NozzleCalibrationOverlay') }}
                             </div>
                         </v-col>
                     </v-row>
-                    <v-row >
+                    <v-row class="mt-0">
                         <v-col class="py-2 col-6">
                             <v-checkbox
                                 v-model="nozzleCalibration"
