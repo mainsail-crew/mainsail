d664b72289eaa338c601e78c86ac5d5511d99b4c
Stefan Dej <meteyou@gmail.com>
2023-08-27 15:33:20 +0200

    feat: add warning for outdated browsers
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

--- src/store/gui/notifications/getters.ts
+++ src/store/gui/notifications/getters.ts
@@ -6,6 +6,8 @@ import { RootStateDependency } from '@/store/types'
 import { sha256 } from 'js-sha256'
 import { PrinterStateKlipperConfigWarning } from '@/store/printer/types'
 import { browserDetect } from '@/plugins/BrowserDetect'
+import semver from 'semver'
+import { minBrowserVersions } from '@/store/variables'
 
 export const getters: GetterTree<GuiNotificationState, any> = {
     getNotifications: (state, getters) => {
@@ -23,7 +25,7 @@ export const getters: GetterTree<GuiNotificationState, any> = {
         // moonraker warnings
         notifications = notifications.concat(getters['getNotificationsMoonrakerWarnings'])
 
-        // moonraker failed compontents
+        // moonraker failed components
         notifications = notifications.concat(getters['getNotificationsMoonrakerFailedComponents'])
 
         // klipper warnings
@@ -274,10 +276,38 @@ export const getters: GetterTree<GuiNotificationState, any> = {
         return notifications
     },
 
-    getNotificationsBrowserWarnings: (state, getters, rootState, rootGetters) => {
+    getNotificationsBrowserWarnings: (state, getters, rootState) => {
         const notifications: GuiNotificationStateEntry[] = []
 
-        window.console.log(browserDetect())
+        const browser = browserDetect()
+        const date = rootState.server.system_boot_at ?? new Date()
+
+        const minBrowserVersion = minBrowserVersions.find(
+            (entry) => entry.name.toLowerCase() === browser.name.toLowerCase()
+        )
+        // stop here, because no browser requirement found
+        if (minBrowserVersion === undefined) return notifications
+
+        if (
+            semver.valid(browser.version) &&
+            semver.valid(minBrowserVersion.version) &&
+            semver.gt(minBrowserVersion.version, browser.version)
+        ) {
+            notifications.push({
+                id: `browserWarning/${minBrowserVersion.name}/${minBrowserVersion.version}`,
+                priority: 'critical',
+                title: i18n.t('App.Notifications.BrowserWarnings.Headline').toString(),
+                description: i18n
+                    .t('App.Notifications.BrowserWarnings.Description', {
+                        name: minBrowserVersion.name,
+                        version: browser.version,
+                        minVersion: minBrowserVersion.version,
+                    })
+                    .toString(),
+                date,
+                dismissed: false,
+            } as GuiNotificationStateEntry)
+        }
 
         return notifications
     },
