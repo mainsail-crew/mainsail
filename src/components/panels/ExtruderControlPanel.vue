f1e90846a9fbdddad005f284f45d894e859794a4
HelgeKeck <helgekeck@hotmail.com>
2023-09-20 21:42:17 +0200

    Update ExtruderControlPanel.vue

--- src/components/panels/ExtruderControlPanel.vue
+++ src/components/panels/ExtruderControlPanel.vue
@@ -61,12 +61,26 @@
                         <v-btn
                             v-for="tool in toolchangeMacros"
                             :key="tool.name"
-                            :class="tool.active ? 'primary--text' : {}"
                             :disabled="isPrinting"
-                            :style="{ 'background-color': '#' + tool.color }"
-                            dense
+                            :loading="loadings.includes(tool.name.toLowerCase())"
                             class="flex-grow-1 px-0"
+                            :style="{
+                                'background-color': tool.active
+                                    ? homedAxes.includes('xyz')
+                                        ? primaryColor
+                                        : warningColor
+                                    : '',
+                                color: tool.active ? primaryTextColor : '',
+                            }"
+                            dense
                             @click="doSend(tool.name)">
+                            <span
+                                v-if="tool.color != null"
+                                class="_extruderColorState mr-1"
+                                :style="{
+                                    'border-color': tool.active ? primaryTextColor : '',
+                                    'background-color': '#' + tool.color,
+                                }"></span>
                             {{ tool.name }}
                         </v-btn>
                     </v-item-group>
@@ -481,6 +495,36 @@ export default class ExtruderControlPanel extends Mixins(BaseMixin, ControlMixin
         return this.$store.state.gui.control.extruder.showEstimatedExtrusionInfo
     }
 
+    get primaryColor(): string {
+        return this.$store.state.gui.uiSettings.primary
+    }
+
+    get primaryTextColor(): string {
+        let splits = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(this.primaryColor)
+        if (splits) {
+            const r = parseInt(splits[1], 16) * 0.2126
+            const g = parseInt(splits[2], 16) * 0.7152
+            const b = parseInt(splits[3], 16) * 0.0722
+            const perceivedLightness = (r + g + b) / 255
+
+            return perceivedLightness > 0.7 ? '#222' : '#fff'
+        }
+
+        return '#ffffff'
+    }
+
+    get warningColor(): string {
+        return this.$vuetify?.theme?.currentTheme?.warning?.toString() ?? '#ff8300'
+    }
+
+    get isIdex(): boolean {
+        return 'dual_carriage' in this.$store.state.printer
+    }
+
+    get idexMode(): string {
+        return this.$store.state.printer.dual_carriage?.carriage_1?.toString().toLowerCase()
+    }
+
     @Watch('maxExtrudeOnlyDistance', { immediate: true })
     onMaxExtrudeOnlyDistanceChange(): void {
         /**
@@ -504,10 +548,22 @@ export default class ExtruderControlPanel extends Mixins(BaseMixin, ControlMixin
         this.$store.dispatch('server/addEvent', { message: gcode, type: 'command' })
         this.$socket.emit('printer.gcode.script', { script: gcode }, { loading: 'btnDetract' })
     }
+
+    doSend(gcode: string): void {
+        this.$store.dispatch('server/addEvent', { message: gcode, type: 'command' })
+        this.$socket.emit('printer.gcode.script', { script: gcode }, { loading: gcode.toLowerCase() })
+    }
 }
 </script>
 
 <style lang="scss" scoped>
+._extruderColorState {
+    width: 15px;
+    height: 15px;
+    border-radius: 50%;
+    border: 1px solid lightgray;
+}
+
 ._btn-group {
     border-radius: 4px;
     display: inline-flex;
