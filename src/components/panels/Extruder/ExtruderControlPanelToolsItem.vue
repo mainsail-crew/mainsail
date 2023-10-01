66d493b1ddff8939543c3e30e103e3da4b194d3d
Stefan Dej <meteyou@gmail.com>
2023-10-01 23:24:32 +0200

    refactor: rework tool color in extruder panel
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

--- src/components/panels/Extruder/ExtruderControlPanelToolsItem.vue
+++ src/components/panels/Extruder/ExtruderControlPanelToolsItem.vue
@@ -1,11 +1,11 @@
 <template>
     <v-btn
-        :class="buttonClass"
         :disabled="printerIsPrintingOnly"
-        :style="buttonStyle"
         dense
         class="flex-grow-1 px-0"
+        :style="buttonStyle"
         @click="doSend(macro.name)">
+        <span v-if="color != null" class="_extruderColorState mr-1" :style="dotStyle" />
         {{ macro.name }}
     </v-btn>
 </template>
@@ -27,19 +27,62 @@ export default class ExtruderControlPanel extends Mixins(BaseMixin, ControlMixin
     }
 
     get color() {
-        return this.macro.variables.color ?? this.macro.variables.colour ?? null
+        const color = this.macro.variables.color ?? this.macro.variables.colour ?? null
+        if (color === '' || color === 'undefined') return null
+
+        return color
     }
 
-    get buttonClass() {
-        return {
-            'primary--text': this.active,
+    get primaryColor(): string {
+        return this.$store.state.gui.uiSettings.primary
+    }
+
+    get primaryTextColor(): string {
+        let splits = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(this.primaryColor)
+        if (splits) {
+            const r = parseInt(splits[1], 16) * 0.2126
+            const g = parseInt(splits[2], 16) * 0.7152
+            const b = parseInt(splits[3], 16) * 0.0722
+            const perceivedLightness = (r + g + b) / 255
+
+            return perceivedLightness > 0.7 ? '#222' : '#fff'
         }
+
+        return '#ffffff'
+    }
+
+    get warningColor(): string {
+        return this.$vuetify?.theme?.currentTheme?.warning?.toString() ?? '#ff8300'
     }
 
     get buttonStyle() {
+        let backgroundColor = ''
+        if (this.active) {
+            backgroundColor = this.homedAxes.includes('xyz') ? this.primaryColor : this.warningColor
+        }
+
+        const textColor = this.active ? this.primaryTextColor : ''
+
         return {
+            color: textColor,
+            'background-color': backgroundColor,
+        }
+    }
+
+    get dotStyle() {
+        return {
+            'border-color': this.active ? this.primaryTextColor : '',
             'background-color': '#' + this.color,
         }
     }
 }
 </script>
+
+<style lang="scss" scoped>
+._extruderColorState {
+    width: 15px;
+    height: 15px;
+    border-radius: 50%;
+    border: 1px solid lightgray;
+}
+</style>
