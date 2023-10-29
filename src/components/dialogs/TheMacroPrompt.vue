b560dff75781b2c70521d3c1796384b80a4370f6
Stefan Dej <meteyou@gmail.com>
2023-10-29 12:54:36 +0100

    fix: fix some issues after refactoring
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

--- src/components/dialogs/TheMacroPrompt.vue
+++ src/components/dialogs/TheMacroPrompt.vue
@@ -61,24 +61,24 @@ export default class TheMacroPrompt extends Mixins(BaseMixin) {
     }
 
     get actions() {
-        return this.events.filter((event: ServerStateEvent) => event.type === 'action')
+        return this.events.filter(
+            (event: ServerStateEvent) => event.type === 'action' && event.message.startsWith('// action:prompt_')
+        )
     }
 
     get macroPromptEvents() {
-        return this.actions
-            .filter((event: ServerStateEvent) => event.message.startsWith('// action:prompt_'))
-            .map((event: ServerStateEvent) => {
-                const type = event.message.replace('// action:prompt_', '').split(' ')[0].trim()
-                const message = (event.message ?? '').replace(`// action:prompt_${type}`, '').replace(/"/g, '').trim()
+        return this.actions.map((event: ServerStateEvent) => {
+            const type = (event.message ?? '').replace('// action:prompt_', '').split(' ')[0].trim()
+            const message = (event.message ?? '').replace(`// action:prompt_${type}`, '').replace(/"/g, '').trim()
 
-                const promptContent: ServerStateEventPromptContent = {
-                    date: event.date,
-                    type,
-                    message,
-                }
+            const promptContent: ServerStateEventPrompt = {
+                date: event.date,
+                type,
+                message,
+            }
 
-                return promptContent
-            })
+            return promptContent
+        })
     }
 
     get lastPromptBeginPos() {
@@ -128,7 +128,7 @@ export default class TheMacroPrompt extends Mixins(BaseMixin) {
                 date: buttons[0].date,
                 type: 'button_group',
                 message: '',
-                children: buttons,
+                children: buttons.filter((event: ServerStateEventPrompt) => event.type === 'button'),
             }
 
             output.splice(start + 1, buttons.length + 1)
@@ -140,7 +140,7 @@ export default class TheMacroPrompt extends Mixins(BaseMixin) {
     get headline() {
         if (!this.showDialog || this.lastPromptBeginPos === -1) return ''
 
-        return this.activePrompt[this.lastPromptBeginPos].message ?? ''
+        return this.macroPromptEvents[this.lastPromptBeginPos]?.message ?? ''
     }
 
     get footerButtons() {
