cc8ff26279916a7e5fe10cb7bd024b41cee13c9b
Patrick Stainbrook <patrick@hohestadt.de>
2023-11-02 23:21:45 +0100

    fix: remove echo from console msgs
    Signed-off-by: Patrick Stainbrook <patrick@hohestadt.de>

--- src/plugins/helpers.ts
+++ src/plugins/helpers.ts
@@ -70,6 +70,9 @@ export function formatConsoleMessage(message: string): string {
     message = message.replace(/^\/\/ /g, '')
     // remove // at \n new line
     message = message.replace(/\n\/\/ /g, '\n')
+    // remove echo
+    message = message.replace(/^echo:/g, '')
+    message = message.replace(/^echo: /g, '')
     // replace linebreaks with html <br>
     message = message.replace('\n// ', '<br>')
     message = message.replace(/\r\n|\r|\n/g, '<br>')
