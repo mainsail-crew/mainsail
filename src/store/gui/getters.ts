8330b4a1024c40d540395a7db67f4a3dec3578a7
Stefan Dej <meteyou@gmail.com>
2023-11-18 11:58:49 +0100

    fix: fix ETA 12hour detection if the user use default browser settings
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

--- src/store/gui/getters.ts
+++ src/store/gui/getters.ts
@@ -157,8 +157,9 @@ export const getters: GetterTree<GuiState, any> = {
         const setting = state.general.timeFormat
         if (setting === '12hours') return true
         if (setting === null) {
-            const timestring = new Date().toLocaleTimeString().split(' ')
-            return timestring.includes('AM') || timestring.includes('PM')
+            // create a time string, cut the last 2 chars and check if it contains AM or PM
+            const timeString = new Date().toLocaleString(navigator.language, { timeStyle: 'short' }).slice(-2)
+            if (['AM', 'PM'].includes(timeString)) return true
         }
 
         return false
