b1a698edad90cbc717d2f704ed1d8b105aa64677
Stefan Dej <meteyou@gmail.com>
2023-12-25 21:07:44 +0100

    fix: fix tooltip of tempchart
    the tooltip will not go away after 1 second. the chart also will be "stopped" as long as your mouse is hoverd
    
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

--- src/components/charts/TempChart.vue
+++ src/components/charts/TempChart.vue
@@ -6,7 +6,9 @@
         :init-options="{ renderer: 'svg' }"
         :autoresize="true"
         :style="tempchartStyle"
-        class="tempchart" />
+        class="tempchart"
+        @mouseenter.native="hoverChart = true"
+        @mouseleave.native="hoverChart = false" />
 </template>
 
 <script lang="ts">
@@ -34,6 +36,7 @@ export default class TempChart extends Mixins(BaseMixin, ThemeMixin) {
         tempchart: any
     }
 
+    hoverChart = false
     private isVisible = true
     get chartOptions(): ECBasicOption {
         return {
@@ -69,7 +72,9 @@ export default class TempChart extends Mixins(BaseMixin, ThemeMixin) {
             },
             yAxis: this.yAxis,
             media: this.media,
-            dataset: { source: this.source },
+            dataset: {
+                source: [],
+            },
             series: this.series,
         }
     }
@@ -273,9 +278,12 @@ export default class TempChart extends Mixins(BaseMixin, ThemeMixin) {
     tooltipFormatter(datasets: any) {
         let output = ''
 
+        //window.console.log('tempchartFix', datasets)
+
         const mainDatasets = datasets.filter((dataset: any) => dataset.seriesName.endsWith('-temperature'))
         if (datasets.length) {
             let outputTime = datasets[0]['axisValueLabel']
+            //window.console.log('tempchartFix', outputTime)
             outputTime = outputTime.substring(outputTime.indexOf(' '))
             const theme = this.$vuetify.theme.dark ? 'theme-dark' : ''
 
@@ -355,8 +363,14 @@ export default class TempChart extends Mixins(BaseMixin, ThemeMixin) {
 
     @Watch('source')
     sourceChanged(newVal: PrinterTempHistoryStateSourceEntry[]) {
-        // break if chart isn't initialized or not visible
-        if (!this.chart || !this.isVisible) return
+        // break if the chart isn't initialized or not visible or is hovered
+        if (!this.chart || !this.isVisible || this.hoverChart) return
+
+        this.chart?.setOption({
+            dataset: {
+                source: newVal,
+            },
+        })
 
         const limitDate = new Date(Date.now() - this.maxHistory * 1000)
         let newSource = newVal.filter((entry: PrinterTempHistoryStateSourceEntry) => {
