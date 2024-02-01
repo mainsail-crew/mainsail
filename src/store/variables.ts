a8c098a09a956236a60664433b46d6c41d649fd7
Stefan Dej <meteyou@gmail.com>
2024-02-01 19:53:39 +0100

    chore: update moonraker min version to v0.8.0-306
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

--- src/store/variables.ts
+++ src/store/variables.ts
@@ -4,7 +4,7 @@ export const defaultPrimaryColor = '#2196f3'
 export const defaultBigThumbnailBackground = '#1e1e1e'
 
 export const minKlipperVersion = 'v0.11.0-257'
-export const minMoonrakerVersion = 'v0.8.0-137'
+export const minMoonrakerVersion = 'v0.8.0-306'
 export const minBrowserVersions = [{ name: 'safari', version: '16.5.2' }]
 
 export const colorArray = ['#F44336', '#8e379d', '#03DAC5', '#3F51B5', '#ffde03', '#009688', '#E91E63']
