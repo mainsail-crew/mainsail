4785f125aa37cfed8dea529f96781fd6962a83b9
Patrick Stainbrook <patrick@hohestadt.de>
2024-05-27 10:18:51 +0200

    style: run prettier... again...
    Signed-off-by: Patrick Stainbrook <patrick@hohestadt.de>

--- src/components/panels/Miscellaneous/MoonrakerSensor.vue
+++ src/components/panels/Miscellaneous/MoonrakerSensor.vue
@@ -4,13 +4,13 @@
             <v-col class="pb-3">
                 <v-subheader class="sensor-name _moonraker-sensor-subheader">
                     {{ convertName(name) }}
-                </v-subheader>                    
-                <v-subheader class="_moonraker-sensor-subheader">                    
-                        <moonraker-sensor-value
+                </v-subheader>
+                <v-subheader class="_moonraker-sensor-subheader">
+                    <moonraker-sensor-value
                         v-for="(valueName, index) of valueNames"
                         :key="'moonraker_sensor_value_' + index"
                         :sensor="name"
-                        :value-name="valueName" />                                                                                                                         
+                        :value-name="valueName" />
                 </v-subheader>
             </v-col>
         </v-row>
@@ -22,9 +22,7 @@ import { convertName } from '@/plugins/helpers'
 import { Component, Mixins, Prop } from 'vue-property-decorator'
 import BaseMixin from '@/components/mixins/base'
 import MoonrakerSensorValue from '@/components/panels/Miscellaneous/MoonrakerSensorValue.vue'
-import {
-
-} from '@mdi/js'
+import {} from '@mdi/js'
 
 @Component({
     components: { MoonrakerSensorValue },
@@ -51,7 +49,6 @@ export default class MoonrakerSensor extends Mixins(BaseMixin) {
 
 .sensor-name {
     font-weight: bold;
-    margin-bottom: 5px;     
+    margin-bottom: 5px;
 }
-
 </style>
