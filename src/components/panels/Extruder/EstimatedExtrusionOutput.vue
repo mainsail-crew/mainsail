3fb988af9232801dd9038d79d590c09665f4c5df
Stefan Dej <meteyou@gmail.com>
2024-06-23 14:09:50 +0200

    fix(extruderPanel): add speed_factor to estimate extrusion calc (#1913)

--- src/components/panels/Extruder/EstimatedExtrusionOutput.vue
+++ src/components/panels/Extruder/EstimatedExtrusionOutput.vue
@@ -8,6 +8,21 @@
                     {{ mdiDiameterVariant }}
                 </v-icon>
                 {{ nozzleDiameter }} mm
+                <v-tooltip v-if="showTooltip" top>
+                    <template #activator="{ on, attrs }">
+                        <v-icon small color="warning" v-bind="attrs" v-on="on">
+                            {{ mdiInformationOutline }}
+                        </v-icon>
+                    </template>
+                    <span>
+                        <div v-if="speed_factor !== 1">
+                            {{ $t('Panels.ToolheadControlPanel.SpeedFactor') }}: {{ speed_factor * 100 }} %
+                        </div>
+                        <div v-if="extrudeFactor !== 1">
+                            {{ $t('Panels.ExtruderControlPanel.ExtrusionFactor') }}: {{ extrudeFactor * 100 }} %
+                        </div>
+                    </span>
+                </v-tooltip>
             </span>
         </div>
     </v-container>
@@ -16,12 +31,13 @@
 <script lang="ts">
 import { Component, Mixins } from 'vue-property-decorator'
 import BaseMixin from '@/components/mixins/base'
-import { mdiDiameterVariant } from '@mdi/js'
+import { mdiDiameterVariant, mdiInformationOutline } from '@mdi/js'
 import ExtruderMixin from '@/components/mixins/extruder'
 
 @Component({})
 export default class PressureAdvanceSettings extends Mixins(BaseMixin, ExtruderMixin) {
     mdiDiameterVariant = mdiDiameterVariant
+    mdiInformationOutline = mdiInformationOutline
 
     get showEstimatedExtrusion() {
         return this.$store.state.gui.control.extruder.showEstimatedExtrusionInfo ?? true
@@ -35,8 +51,18 @@ export default class PressureAdvanceSettings extends Mixins(BaseMixin, ExtruderM
         )
     }
 
+    get speed_factor() {
+        return this.$store.state.printer.gcode_move?.speed_factor ?? 1
+    }
+
     get volumetricFlow(): number {
-        return Math.round(Math.pow(this.filamentDiameter / 2, 2) * Math.PI * this.feedrate * 10) / 10
+        return (
+            Math.round(Math.pow(this.filamentDiameter / 2, 2) * Math.PI * this.feedrate * this.speed_factor * 10) / 10
+        )
+    }
+
+    get showTooltip() {
+        return this.speed_factor !== 1 || this.extrudeFactor !== 1
     }
 }
 </script>
