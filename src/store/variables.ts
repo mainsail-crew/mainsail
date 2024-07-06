55d446d176e91db6df63dba9ca7e7ef19572ede0
Stefan Dej <meteyou@gmail.com>
2024-07-06 22:50:51 +0200

    feat(theme): add bigtreetech theme
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

--- src/store/variables.ts
+++ src/store/variables.ts
@@ -155,4 +155,10 @@ export const themes: Theme[] = [
         colorLogo: '#b12f35',
         logo: { show: true, light: false },
     },
+    {
+        name: 'btt',
+        displayName: 'BigTreeTech (Sponsor)',
+        colorLogo: '#ef0025',
+        logo: { show: true, light: false },
+    },
 ]
