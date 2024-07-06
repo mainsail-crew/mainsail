ad9e71b4cd96e8ee6c10c799cf352042bb0da98b
Stefan Dej <meteyou@gmail.com>
2024-07-06 22:57:57 +0200

    feat(theme): add voron build-in theme (#1930)

--- src/store/variables.ts
+++ src/store/variables.ts
@@ -155,4 +155,10 @@ export const themes: Theme[] = [
         colorLogo: '#b12f35',
         logo: { show: true, light: false },
     },
+    {
+        name: 'voron',
+        displayName: 'Voron Design',
+        colorLogo: '#FF2300',
+        logo: { show: true, light: false },
+    },
 ]
