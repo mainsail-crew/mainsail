04433fc397943b6858bb6dc59f4447a1b28e58e2
Stefan Dej <meteyou@gmail.com>
2024-12-01 21:38:55 +0100

    feat(Webcam): add a optional overlay for IDEX calibration (#2053)

--- src/components/webcams/streamers/Mjpegstreamer.vue
+++ src/components/webcams/streamers/Mjpegstreamer.vue
@@ -12,6 +12,7 @@
         <span v-if="showFpsCounter && status === 'connected'" class="webcamFpsOutput">
             {{ $t('Panels.WebcamPanel.FPS') }}: {{ fpsOutput }}
         </span>
+        <webcam-nozzle-crosshair v-if="showNozzleCrosshair" :webcam="camSettings" />
         <v-row v-if="status !== 'connected'">
             <v-col class="_webcam_mjpegstreamer_output text-center d-flex flex-column justify-center align-center">
                 <v-progress-circular v-if="status === 'connecting'" indeterminate color="primary" class="mb-3" />
@@ -98,6 +99,12 @@ export default class Mjpegstreamer extends Mixins(BaseMixin, WebcamMixin) {
         if (this.page !== 'dashboard') return true
 
         return this.$store.getters['gui/getPanelExpand']('webcam-panel', this.viewport) ?? false
+    }
+
+    get showNozzleCrosshair() {
+        const nozzleCrosshair = this.camSettings.extra_data?.nozzleCrosshair ?? false
+
+        return nozzleCrosshair && this.status === 'connected'
     }
 
     // start or stop the video when the expanded state changes
