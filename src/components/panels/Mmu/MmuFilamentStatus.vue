1e2cf1b83da6f2e06d1ba8ff0c5f085aacd794e9
moggieuk <32878385+moggieuk@users.noreply.github.com>
2025-12-17 09:22:09 +0700

    Update src/components/panels/Mmu/MmuFilamentStatus.vue
    Co-authored-by: Stefan Dej <meteyou@gmail.com>

--- src/components/panels/Mmu/MmuFilamentStatus.vue
+++ src/components/panels/Mmu/MmuFilamentStatus.vue
@@ -19,7 +19,7 @@
                 <rect x="3" y="0" width="30" height="40" rx="3" ry="3" fill="none" stroke-width="1.5" />
                 <path d="M-15 -4 L-6 0 L-15 4 Z" stroke-width="1" fill-opacity="0.6" />
                 <path d="M8 40 L 28 40" stroke-width="4" />
-                <text x="-22" y="4" font-size="11px" text-anchor="end" style="fill: var(--color-outline)">
+                <text v-if="hasFilamentProportionalSensor" x="-22" y="4" font-size="11px" text-anchor="end" style="fill: var(--color-outline)">
                     {{ syncFeedbackPistonText }}
                 </text>
             </g>
