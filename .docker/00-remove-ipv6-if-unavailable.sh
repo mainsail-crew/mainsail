9426d3386275eab603c7b2631c920a311157d949
Stefan Dej <meteyou@gmail.com>
2025-12-20 22:22:32 +0100

    fix: also check, if the file is not empty
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

--- .docker/00-remove-ipv6-if-unavailable.sh
+++ .docker/00-remove-ipv6-if-unavailable.sh
@@ -6,8 +6,8 @@ set -e
 ME=$(basename "$0")
 DEFAULT_CONF_FILE="/etc/nginx/conf.d/default.conf"
 
-# Check if IPv6 is available
-if [ ! -f "/proc/net/if_inet6" ]; then
+# Check if IPv6 is available and not empty
+if [ ! -f "/proc/net/if_inet6" ] || [ ! -s "/proc/net/if_inet6" ]; then
     echo "$ME: info: IPv6 not available, removing IPv6 listen directives"
     sed -i '/listen \[::\]:/d' "$DEFAULT_CONF_FILE"
 fi
