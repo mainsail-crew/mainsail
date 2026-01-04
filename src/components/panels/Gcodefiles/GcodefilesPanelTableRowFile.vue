5cc6e441900803488bc91a43886e804c043f9217
Stefan Dej <meteyou@gmail.com>
2026-01-04 22:12:39 +0100

    refactor(Dialogs): replace delete file dialog with confirmation dialog using v-model
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

--- src/components/panels/Gcodefiles/GcodefilesPanelTableRowFile.vue
+++ src/components/panels/Gcodefiles/GcodefilesPanelTableRowFile.vue
@@ -119,10 +119,13 @@
             :item="item"
             :show-dialog="showDuplicateFileDialog"
             @close="showDuplicateFileDialog = false" />
-        <gcodefiles-delete-file-dialog
-            :item="item"
-            :show-dialog="showDeleteFileDialog"
-            @close="showDeleteFileDialog = false" />
+        <confirmation-dialog
+            v-model="showDeleteFileDialog"
+            :title="$t('Files.Delete')"
+            :text="$t('Files.DeleteSingleFileQuestion', { name: item.filename })"
+            :action-button-text="$t('Files.Delete')"
+            :cancel-button-text="$t('Files.Cancel')"
+            @action="deleteFile" />
     </tr>
 </template>
 <script lang="ts">
@@ -148,6 +151,7 @@ import ControlMixin from '@/components/mixins/control'
 import { convertPrintStatusIcon, convertPrintStatusIconColor, escapePath } from '@/plugins/helpers'
 import GcodefilesRenameFileDialog from '@/components/dialogs/GcodefilesRenameFileDialog.vue'
 import GcodefilesDuplicateFileDialog from '@/components/dialogs/GcodefilesDuplicateFileDialog.vue'
+import ConfirmationDialog from '@/components/dialogs/ConfirmationDialog.vue'
 import GcodefilesPanelTableRowFileMetadata from '@/components/panels/Gcodefiles/GcodefilesPanelTableRowFileMetadata.vue'
 import GcodefilesPanelTableRowFileMetadataFilaments from '@/components/panels/Gcodefiles/GcodefilesPanelTableRowFileMetadataFilaments.vue'
 import GcodefilesPanelTableRowFileMetadataSlicer from '@/components/panels/Gcodefiles/GcodefilesPanelTableRowFileMetadataSlicer.vue'
@@ -156,6 +160,7 @@ import { CLOSE_CONTEXT_MENU, EventBus } from '@/plugins/eventBus'
 
 @Component({
     components: {
+        ConfirmationDialog,
         GcodefilesPanelTableRowFileMetadataFilamentStrings,
         GcodefilesPanelTableRowFileMetadataFilaments,
         GcodefilesPanelTableRowFileMetadataSlicer,
@@ -274,6 +279,14 @@ export default class GcodefilesPanelTableRowFile extends Mixins(BaseMixin, Contr
             size: this.item.size,
             permissions: this.item.permissions,
         })
+    }
+
+    deleteFile() {
+        this.$socket.emit(
+            'server.files.delete_file',
+            { path: 'gcodes' + this.currentPath + '/' + this.item.filename },
+            { action: 'files/getDeleteFile' }
+        )
     }
 
     onDragStart(e: DragEvent) {
