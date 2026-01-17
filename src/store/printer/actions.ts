a5bf0d2618808d21e8179fa5ebd53a5e9b94d48e
Stefan Dej <meteyou@gmail.com>
2026-01-17 23:56:46 +0100

    refactor(printer): remove unnecessary webhook handling in payload processing
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

--- src/store/printer/actions.ts
+++ src/store/printer/actions.ts
@@ -81,15 +81,6 @@ export const actions: ActionTree<PrinterState, RootState> = {
         if ('status' in payload) payload = payload.status
         if ('requestParams' in payload) delete payload.requestParams
 
-        if ('webhooks' in payload) {
-            this.dispatch(
-                'server/getData',
-                { klippy_state: payload.webhooks.state, klippy_message: payload.webhooks.state_message },
-                { root: true }
-            )
-            delete payload.webhooks
-        }
-
         if ('bed_mesh' in state && 'bed_mesh' in payload && 'profiles' in payload.bed_mesh) {
             commit('setBedMeshProfiles', payload.bed_mesh.profiles)
 
