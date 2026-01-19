66852d13fdbb1908b90b24013e039cb1cac65168
Stefan Dej <meteyou@gmail.com>
2026-01-19 06:32:43 +0100

    fix: complete MachineRPC interface with all Moonraker methods
    Add missing RPC methods according to Moonraker documentation:
    - machine.sudo.info, machine.sudo.password
    - machine.peripherals.usb, machine.peripherals.serial
    - machine.peripherals.video, machine.peripherals.canbus
    
    Also add missing fields to machine.proc_stats network interface:
    rx_packets, tx_packets, rx_errs, tx_errs, rx_drop, tx_drop
    
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

--- src/types/moonraker/MachineRPC.ts
+++ src/types/moonraker/MachineRPC.ts
@@ -112,6 +112,12 @@ export interface MachineRPC {
             {
                 rx_bytes: number
                 tx_bytes: number
+                rx_packets: number
+                tx_packets: number
+                rx_errs: number
+                tx_errs: number
+                rx_drop: number
+                tx_drop: number
                 bandwidth: number
             }
         >
@@ -162,4 +168,166 @@ export interface MachineRPC {
         /** The name of the service to start */
         service: string
     }) => Promise<'ok'>
+
+    /**
+     * Retrieves sudo information status.
+     * Optionally checks if Moonraker has permission to run commands as root.
+     */
+    'machine.sudo.info': (params?: {
+        /** When true, Moonraker will attempt to run a sudo command to check if sudo permission is available */
+        check_access?: boolean
+    }) => Promise<{
+        /** The result of a requested sudo permission check. Null if check was not requested. */
+        sudo_access: boolean | null
+        /** The name of the linux user the Moonraker process belongs to */
+        linux_user: string
+        /** True if an internal Moonraker component has requested a sudo password */
+        sudo_requested: boolean
+        /** Descriptions of sudo access requests from internal components */
+        request_messages: string[]
+    }>
+
+    /**
+     * Sets the sudo password currently used by Moonraker.
+     * The password is not persistent across Moonraker restarts.
+     */
+    'machine.sudo.password': (params: {
+        /** The linux user password necessary to grant Moonraker sudo permission */
+        password: string
+    }) => Promise<{
+        /** Responses from components that processed sudo requests */
+        sudo_responses: string[]
+        /** True if a processed sudo request intends to restart Moonraker */
+        is_restarting: boolean
+    }>
+
+    /**
+     * Returns a list of all USB devices currently detected on the system.
+     */
+    'machine.peripherals.usb': () => Promise<{
+        /** Array of USB device objects */
+        usb_devices: Array<{
+            /** The USB bus number as reported by the host */
+            bus_num: number
+            /** The USB device number as reported by the host */
+            device_num: number
+            /** Combination of bus and device number as unique location ID */
+            usb_location: string
+            /** The vendor ID as reported by the driver */
+            vendor_id: string
+            /** The product ID as reported by the driver */
+            product_id: string
+            /** The manufacturer name (null if not found) */
+            manufacturer: string | null
+            /** The product description (null if not found) */
+            product: string | null
+            /** The serial number (null if not found) */
+            serial: string | null
+            /** The class description (null if not found) */
+            class: string | null
+            /** The subclass description (null if not found) */
+            subclass: string | null
+            /** The protocol description (null if not found) */
+            protocol: string | null
+            /** Full device description from usb.ids file (null if not found) */
+            description: string | null
+        }>
+    }>
+
+    /**
+     * Returns a list of all serial devices detected on the system.
+     * These may be USB CDC-ACM devices or hardware UARTs.
+     */
+    'machine.peripherals.serial': () => Promise<{
+        /** Array of serial device objects */
+        serial_devices: Array<{
+            /** The type of serial device: 'hardware_uart' or 'usb' */
+            device_type: 'hardware_uart' | 'usb'
+            /** The absolute file path to the device */
+            device_path: string
+            /** The device file name as reported by sysfs */
+            device_name: string
+            /** The name of the device driver */
+            driver_name: string
+            /** Symbolic link based on physical connection (null if not exists) */
+            path_by_hardware: string | null
+            /** Symbolic link based on reported IDs (null if not exists) */
+            path_by_id: string | null
+            /** USB location identifier (null for non-usb devices) */
+            usb_location: string | null
+        }>
+    }>
+
+    /**
+     * Retrieves a list of V4L2 video capture devices on the system.
+     * Also returns libcamera devices if python3-libcamera is installed.
+     */
+    'machine.peripherals.video': () => Promise<{
+        /** Array of V4L2 device objects */
+        v4l2_devices: Array<{
+            /** The V4L2 name assigned to the device */
+            device_name: string
+            /** The absolute system path to the device file */
+            device_path: string
+            /** The camera name reported by the device driver */
+            camera_name: string
+            /** The name of the driver loaded for the device */
+            driver_name: string
+            /** An alternative device name from sysfs (null if not exists) */
+            alt_name: string | null
+            /** A description of the hardware location of the device */
+            hardware_bus: string
+            /** Array of capability strings as reported by V4L2 */
+            capabilities: string[]
+            /** The device version as reported by V4L2 */
+            version: string
+            /** Symbolic link based on physical connection (null if not exists) */
+            path_by_hardware: string | null
+            /** Symbolic link based on reported ID (null if not exists) */
+            path_by_id: string | null
+            /** USB location identifier (null for non-usb devices) */
+            usb_location: string | null
+            /** Array of supported modes with discrete resolutions */
+            modes: Array<{
+                /** The pixel format in V4L2 fourcc format */
+                format: string
+                /** A description of the mode provided by the driver */
+                description: string
+                /** Special condition flags: 'COMPRESSED', 'EMULATED' */
+                flags: string[]
+                /** Array of supported resolutions as 'WIDTHxHEIGHT' strings */
+                resolutions: string[]
+            }>
+        }>
+        /** Array of libcamera device objects */
+        libcamera_devices: Array<{
+            /** The ID of the device as reported by libcamera */
+            libcamera_id: string
+            /** The model name of the device */
+            model: string
+            /** Array of supported modes */
+            modes: Array<{
+                /** The pixel format of the mode */
+                format: string
+                /** Array of supported resolutions as 'WIDTHxHEIGHT' strings */
+                resolutions: string[]
+            }>
+        }>
+    }>
+
+    /**
+     * Queries the provided canbus interface for unassigned Klipper or Katapult node IDs.
+     */
+    'machine.peripherals.canbus': (params?: {
+        /** The cansocket interface to query (default: 'can0') */
+        interface?: string
+    }) => Promise<{
+        /** Array of discovered CAN UUID objects */
+        can_uuids: Array<{
+            /** The UUID of the unassigned node */
+            uuid: string
+            /** The application running on the node: 'Klipper' or 'Katapult' */
+            application: string
+        }>
+    }>
 }
