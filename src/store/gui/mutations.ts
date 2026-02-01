1d8fb2583ce772d8856eef7248b57313ad765536
Stefan Dej <meteyou@gmail.com>
2026-02-01 18:00:41 +0100

    refactor: optimize deepSet function to use Vue.set for state updates
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

--- src/store/gui/mutations.ts
+++ src/store/gui/mutations.ts
@@ -14,13 +14,13 @@ export const mutations: MutationTree<GuiState> = {
     },
 
     saveSetting(state, payload: { name: string; value: any }) {
-        // eslint-disable-next-line
         const deepSet = (obj: any, is: string[] | string, value: any): any => {
             if (is !== undefined && typeof is === 'string') return deepSet(obj, is.split('.'), value)
-            else if (is.length == 1 && value !== undefined) return (obj[is[0]] = value)
-            else if (is.length == 0) return obj
-            else if (!(is[0] in obj)) obj[is[0]] = {}
-            return deepSet(obj[is[0]], is.slice(1), value)
+            else if (is?.length === 1 && value !== undefined) return Vue.set(obj, is[0], value)
+            else if (is?.length === 0) return obj
+            else if (!(is[0] in obj)) Vue.set(obj, is[0], {})
+
+            return deepSet(obj[is[0]], is?.slice(1), value)
         }
 
         deepSet(state, payload.name, payload.value)
