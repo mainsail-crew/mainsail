e41dc89df31fae26dc49d3bdab85e118353e0c42
Stefan Dej <meteyou@gmail.com>
2026-03-01 21:32:08 +0100

    refactor: add an eslint-disable in shims-vue.d.ts to fix no-empty-object-type issue
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

--- src/types/shims-vue.d.ts
+++ src/types/shims-vue.d.ts
@@ -2,6 +2,7 @@
 
 declare module '*.vue' {
     import type { Component } from 'vue'
+    // eslint-disable-next-line @typescript-eslint/no-empty-object-type
     const component: Component<{}, {}, any>
     export default component
 }
