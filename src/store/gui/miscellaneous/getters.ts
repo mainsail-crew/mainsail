e812a65ae878e113df55859544ec9b4d9851e044
Stefan Dej <meteyou@gmail.com>
2026-05-21 06:44:13 +0200

    refactor(store): remove unused getters in gui/miscellaneous store
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

--- src/store/gui/miscellaneous/getters.ts
+++ src/store/gui/miscellaneous/getters.ts
@@ -1,5 +1,5 @@
 import { GetterTree } from 'vuex'
-import { GuiMiscellaneousState, GuiMiscellaneousStateEntry } from '@/store/gui/miscellaneous/types'
+import { GuiMiscellaneousState } from '@/store/gui/miscellaneous/types'
 import { RootState } from '@/store/types'
 
 export const getters: GetterTree<GuiMiscellaneousState, RootState> = {
@@ -13,30 +13,4 @@ export const getters: GetterTree<GuiMiscellaneousState, RootState> = {
                 return entry.type === type && entry.name === name
             })
         },
-
-    getEntries: (state) => {
-        const output: GuiMiscellaneousStateEntry[] = []
-
-        Object.entries(state.entries).forEach(([key, values]) => {
-            output.push({
-                id: key,
-                name: values.name,
-                type: values.type,
-                lightgroups: { ...values.lightgroups },
-                presets: { ...values.presets },
-            })
-        })
-
-        return output
-    },
-
-    getEntry: (state, getters) => (payload: { type: string; name: string }) => {
-        return getters.getEntries.find(
-            (entry: GuiMiscellaneousStateEntry) => entry.name === payload.name && entry.type === payload.type
-        ) as GuiMiscellaneousStateEntry
-    },
-
-    getId: (state, getters) => (payload: { type: string; name: string }) => {
-        return getters.getEntry(payload)?.id ?? null
-    },
 }
