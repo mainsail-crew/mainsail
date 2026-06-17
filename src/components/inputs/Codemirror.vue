1b85076f93c62bbd38b44659c3f322b73ea90931
Sebastian Pietschner <32213671+sebasptsch@users.noreply.github.com>
2026-06-18 00:41:24 +1000

    feat(editor): add YAML support to editor (#2479)
    Co-authored-by: Stefan Dej <meteyou@gmail.com>

--- src/components/inputs/Codemirror.vue
+++ src/components/inputs/Codemirror.vue
@@ -8,19 +8,40 @@
 // Inspired by this repo: https://github.com/surmon-china/vue-codemirror
 
 import { Component, Mixins, Prop, Ref, Watch } from 'vue-property-decorator'
-import BaseMixin from '../mixins/base'
-import ThemeMixin from '../mixins/theme'
+import BaseMixin from '@/components/mixins/base'
+import ThemeMixin from '@/components/mixins/theme'
 import { basicSetup } from 'codemirror'
 import { EditorView, keymap } from '@codemirror/view'
-import { EditorState } from '@codemirror/state'
+import { EditorState, Prec } from '@codemirror/state'
 import { vscodeDark, vscodeLight } from '@uiw/codemirror-theme-vscode'
-import { StreamLanguage } from '@codemirror/language'
+import { HighlightStyle, indentUnit, StreamLanguage, syntaxHighlighting } from '@codemirror/language'
 import { klipper_config } from '@/plugins/StreamParserKlipperConfig'
 import { gcode } from '@/plugins/StreamParserGcode'
 import { insertTab, indentLess } from '@codemirror/commands'
 import { json } from '@codemirror/lang-json'
 import { css } from '@codemirror/lang-css'
-import { indentUnit } from '@codemirror/language'
+import { yaml, yamlLanguage } from '@codemirror/lang-yaml'
+import { tags } from '@lezer/highlight'
+
+const yamlDarkHighlightStyle = HighlightStyle.define(
+    [
+        {
+            tag: tags.definition(tags.propertyName),
+            color: '#dcdcaa',
+        },
+    ],
+    { scope: yamlLanguage, themeType: 'dark' }
+)
+
+const yamlLightHighlightStyle = HighlightStyle.define(
+    [
+        {
+            tag: tags.definition(tags.propertyName),
+            color: '#795e26',
+        },
+    ],
+    { scope: yamlLanguage, themeType: 'light' }
+)
 
 @Component
 export default class Codemirror extends Mixins(BaseMixin, ThemeMixin) {
@@ -104,7 +125,13 @@ export default class Codemirror extends Mixins(BaseMixin, ThemeMixin) {
         if (['cfg', 'conf'].includes(this.fileExtension)) extensions.push(StreamLanguage.define(klipper_config))
         else if (['gcode'].includes(this.fileExtension)) extensions.push(StreamLanguage.define(gcode))
         else if (['json'].includes(this.fileExtension)) extensions.push(json())
-        else if (['css', 'scss', 'sass'].includes(this.fileExtension)) extensions.push(css())
+        else if (['yaml', 'yml'].includes(this.fileExtension)) {
+            extensions.push(
+                yaml(),
+                Prec.highest(syntaxHighlighting(yamlDarkHighlightStyle)),
+                Prec.highest(syntaxHighlighting(yamlLightHighlightStyle))
+            )
+        } else if (['css', 'scss', 'sass'].includes(this.fileExtension)) extensions.push(css())
 
         return extensions
     }
