aae25bb3df5fab55159da6145a8ff489afa35ba5
Stefan Dej <meteyou@gmail.com>
2026-07-26 12:12:10 +0200

    refactor(presets): refactor some code parts for better readability

--- src/components/settings/Presets/PresetsForm.vue
+++ src/components/settings/Presets/PresetsForm.vue
@@ -139,10 +139,8 @@ export default class PresetsForm extends Mixins(BaseMixin) {
     }
 
     existsPresetName(name: string) {
-        return (
-            this.presets.findIndex(
-                (preset: GuiPresetsStatePreset) => preset.name === name && preset.id !== this.preset.id
-            ) !== -1
+        return this.presets.some(
+            (preset: GuiPresetsStatePreset) => preset.name === name && preset.id !== this.preset.id
         )
     }
 
@@ -156,9 +154,9 @@ export default class PresetsForm extends Mixins(BaseMixin) {
 
     savePreset() {
         let setValues = 0
-        for (const key of Object.keys(this.preset.values)) {
+        Object.keys(this.preset.values).forEach((key: string) => {
             if (this.preset.values[key].bool) setValues++
-        }
+        })
         if (this.preset.gcode.length) setValues++
 
         // stop here, when no values are set
