4ef5fda69e3af1af0f3efdf61778086138aff1fa
Stefan Dej <meteyou@gmail.com>
2026-09-03 22:01:48 +0200

    fix(editor): cut shadow on the top from the sidebar

--- src/components/TheEditor.vue
+++ src/components/TheEditor.vue
@@ -537,6 +537,7 @@ export default class TheEditor extends Mixins(BaseMixin) {
     max-height: calc(100vh - 48px);
     border-left: 1px solid rgba(255, 255, 255, 0.12);
     box-shadow: -4px 0 8px rgba(0, 0, 0, 0.35);
+    clip-path: inset(0 0 0 -12px);
 }
 
 .theme--light .structure-sidebar {
